ehsan/wesnoth

commit 06bc23d61a1196f5f7031240cdc82f7edceec5ea

Really fixed bug
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@9837 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -841,7 +841,7 @@ void unit::read(const game_data& data, const config& cfg)
 	}
 
 	const std::string& hitpoints = cfg["hitpoints"];
-	hitpoints_ = lexical_cast_default<int>(hitpoints,0);
+	hitpoints_ = lexical_cast_default<int>(hitpoints,maxHitpoints_);
 	
 	const config* const modifications = cfg.child("modifications");
 	if(modifications != NULL) {
@@ -849,10 +849,6 @@ void unit::read(const game_data& data, const config& cfg)
 		apply_modifications();
 	}
 	
-	if(hitpoints != "") {
-		hitpoints_ = maxHitpoints_;
-	}
-
 	goto_.x = atoi(cfg["goto_x"].c_str()) - 1;
 	goto_.y = atoi(cfg["goto_y"].c_str()) - 1;
 

commit cc562bb0c4c2ebf8533e5b903b9859fc5a88da71

Undo resilient snapshot load breakage caused by r9837, and finally close bug #4962 good and dead. (Resilient adds 10%, then 3hp.  hitpoints_ gets reduced to maxHitpoints_ after the first effect, leaving us 3hp short).
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@9985 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -840,18 +840,18 @@ void unit::read(const game_data& data, const config& cfg)
 		variables_.clear();
 	}
 
-	const std::string& hitpoints = cfg["hitpoints"];
-	hitpoints_ = lexical_cast_default<int>(hitpoints,maxHitpoints_);
-	
+	// Set this to some sane value: add_modification clamps it when
+	// altering maxHitpoints_, and if we don't set it valgrind
+	// complains.  We actually set it below once maxHitpoints_ is stable.
+	hitpoints_ = 1;
 	const config* const modifications = cfg.child("modifications");
 	if(modifications != NULL) {
 		modifications_ = *modifications;
 		apply_modifications();
 	}
 	
-	if(hitpoints == "") {
-		hitpoints_ = maxHitpoints_;
-	}
+	const std::string& hitpoints = cfg["hitpoints"];
+	hitpoints_ = lexical_cast_default<int>(hitpoints,maxHitpoints_);
 	
 	goto_.x = atoi(cfg["goto_x"].c_str()) - 1;
 	goto_.y = atoi(cfg["goto_y"].c_str()) - 1;