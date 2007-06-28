ehsan/wesnoth

commit 93518edf4b85d9d920b8c5ac00bc0cb2990bd2b1

The display class keeps its own viewpoint field now.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@18457 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -64,7 +64,7 @@ namespace {
 }
 
 map_display::map_display(CVideo& video, const gamemap& map, const config& theme_cfg, const config& cfg, const config& level) : 
-	screen_(video), map_(map), xpos_(0), ypos_(0),
+	screen_(video), map_(map), viewpoint_(NULL), xpos_(0), ypos_(0),
 	theme_(theme_cfg,screen_area()), zoom_(DefaultZoom),
 	builder_(cfg, level, map),
 	minimap_(NULL), redrawMinimap_(false), redraw_background_(true),
@@ -1026,7 +1026,7 @@ surface map_display::get_minimap(int w, int h)
 	}
 
 	if(minimap_ == NULL) {
-		minimap_ = image::getMinimap(w, h, map_);
+		minimap_ = image::getMinimap(w, h, map_, viewpoint_);
 	}
 
 	return minimap_;
@@ -2598,13 +2598,14 @@ const std::string display::current_team_name() const
 	return std::string();
 }
 
-void display::set_team(size_t team)
+void display::set_team(size_t teamindex)
 {
-	wassert(team < teams_.size());
-	currentTeam_ = team;
+	wassert(teamindex < teams_.size());
+	currentTeam_ = teamindex;
 	if (!is_observer())
 	{
-		labels().set_team(&teams_[team]);
+		labels().set_team(&teams_[teamindex]);
+		viewpoint_ = &teams_[teamindex];
 	}
 	else
 	{
@@ -2613,10 +2614,10 @@ void display::set_team(size_t team)
 	labels().recalculate_labels();
 }
 
-void display::set_playing_team(size_t team)
+void display::set_playing_team(size_t teamindex)
 {
-	wassert(team < teams_.size());
-	activeTeam_ = team;
+	wassert(teamindex < teams_.size());
+	activeTeam_ = teamindex;
 	invalidate_game_status();
 }
 
@@ -2767,20 +2768,5 @@ void display::prune_chat_messages(bool remove_all)
 		prune_chat_messages(remove_all);
 	}
 }
-
-surface display::get_minimap(int w, int h)
-{
-	if(minimap_ != NULL && (minimap_->w != w || minimap_->h != h)) {
-		minimap_ = NULL;
-	}
-
-	const team *tm = team_valid() ? &teams_[currentTeam_] : NULL;
-	if(minimap_ == NULL) {
-		minimap_ = image::getMinimap(w, h, map_, tm);
-	}
-
-	return minimap_;
-}
-
 
 display *display::singleton_ = NULL;

src/display.hpp
@@ -140,6 +140,13 @@ public:
 	int get_location_y(const gamemap::location& loc) const
 		{ return map_area().y + (loc.y + 1) * zoom_ - ypos_ + (is_odd(loc.x) ? zoom_/2 : 0); }
 
+	//function which returns true if location (x,y) is covered in shroud.
+	bool shrouded(const gamemap::location& loc) const
+		{return !viewpoint_ || viewpoint_->shrouded(loc.x, loc.y);}
+	//function which returns true if location (x,y) is covered in fog.
+	bool fogged(const gamemap::location& loc) const
+		{return !viewpoint_ || viewpoint_->fogged(loc.x, loc.y);}
+
 	//function which determines whether a grid should be overlayed on the
 	//game board to more clearly show where hexes are.
 	void set_grid(const bool grid) { grid_ = grid; }
@@ -158,12 +165,10 @@ public:
 	void invalidate_theme() { panelsDrawn_ = false; }
 
 	// Will be overridden in the display subclass
-	virtual bool fogged(const gamemap::location& loc UNUSED) const {return false;};
-	virtual bool shrouded(const gamemap::location& loc UNUSED) const {return false;};
 	virtual void invalidate(const gamemap::location& loc) {invalidated_.insert(loc);};
 	virtual void draw_minimap_units(int x UNUSED, int y UNUSED, int w UNUSED, int h UNUSED) {};
 	//this surface must be freed by the caller
-	virtual surface get_minimap(int w, int h);
+	surface get_minimap(int w, int h);
 
 	const gamemap& get_map()const { return map_;}
 
@@ -255,6 +260,7 @@ protected:
 protected:
 	CVideo& screen_;
 	const gamemap& map_;
+	const viewpoint *viewpoint_;
 	int xpos_, ypos_;
 	theme theme_;
 	int zoom_;
@@ -479,17 +485,11 @@ public:
 	static void debug_highlight(const gamemap::location& loc, fixed_t amount);
 	static void clear_debug_highlights() { debugHighlights_.clear(); }
 
-	//function which returns true if location (x,y) is covered in shroud.
-	bool shrouded(const gamemap::location& loc) const
-		{ return team_valid() ? teams_[currentTeam_].shrouded(loc.x, loc.y) : false; }
-
-	bool fogged(const gamemap::location& loc) const
-		{ return team_valid() ? teams_[currentTeam_].fogged(loc.x, loc.y) : false; }
-
 	//the viewing team is the team currently viewing the game. The
 	//playing team is the team whose turn it is
 	size_t viewing_team() const { return currentTeam_; }
 	size_t playing_team() const { return activeTeam_; }
+
 	bool team_valid() const { return currentTeam_ < teams_.size(); }
 	const std::string current_team_name() const;
 
@@ -534,9 +534,6 @@ private:
 
 	//this surface must be freed by the caller
 	surface get_flag(const t_translation::t_letter& terrain, const gamemap::location& loc);
-
-	//this surface must be freed by the caller
-	virtual surface get_minimap(int w, int h);
 
 	CKey keys_;
 

commit 0e52a8c784a2bad9baa7454676c38fa03cd4be0c

Fix a minimap MP bug introduced in r18457.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@18471 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -2665,6 +2665,7 @@ void display::set_team(size_t teamindex)
 	else
 	{
 		labels().set_team(0);
+		viewpoint_ = NULL;
 	}
 	labels().recalculate_labels();
 }