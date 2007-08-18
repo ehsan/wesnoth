ehsan/wesnoth

commit 08d95f1dbe87425f0350ab7df663bf033346097b

when MOVE_VIEWPORT is set the menu should reposition the viewport even if the selection is not changed
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@19617 75b3775d-3801-0410-9e12-e2e43121521d

src/widgets/menu.cpp
@@ -486,12 +486,14 @@ void menu::move_selection_down(size_t dep)
 
 void menu::set_selection_pos(size_t new_selected, SELECTION_MOVE_VIEWPORT move_viewport)
 {
-	if (new_selected == selected_ || new_selected >= items_.size())
+	if (new_selected >= items_.size())
 		return;
 
-	invalidate_row_pos(selected_);
-	invalidate_row_pos(new_selected);
-	selected_ = new_selected;
+	if (new_selected != selected_) {
+		invalidate_row_pos(selected_);
+		invalidate_row_pos(new_selected);
+		selected_ = new_selected;
+	}
 
 	if(move_viewport == MOVE_VIEWPORT) {
 		adjust_viewport_to_selection();

commit 8494403abfb664901b89674921787b0625dc0573

Fix a sound bug introduced in r19617: The mouseover-highlighting of selection for in-game menu causes repetitive UI sounds. I think this fix preserves the wanted interface change. but, uso, maybe check if the sound works as you want where(?) you want
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@19633 75b3775d-3801-0410-9e12-e2e43121521d

src/widgets/menu.cpp
@@ -489,15 +489,17 @@ void menu::set_selection_pos(size_t new_selected, SELECTION_MOVE_VIEWPORT move_v
 	if (new_selected >= items_.size())
 		return;
 
+	bool changed = false;
 	if (new_selected != selected_) {
 		invalidate_row_pos(selected_);
 		invalidate_row_pos(new_selected);
 		selected_ = new_selected;
+		changed = true;
 	}
 
 	if(move_viewport == MOVE_VIEWPORT) {
 		adjust_viewport_to_selection();
-		if(!silent_) {
+		if(!silent_ && changed) {
 			sound::play_UI_sound(game_config::sounds::menu_select);
 		}
 	}