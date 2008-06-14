ehsan/wesnoth

commit ccad38292ab0932ec4385b743cf986e7a89eb1fe

-Make get_binary_file_location() search files only, not directory. Because it make more sense with the function's name and our current use of it. This also skip some ugly win32 system calls done for each file search. -Adapt the "[textdomain] path=..." which was the only use of "binary" directory (and add some specific logging for this part)
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27176 75b3775d-3801-0410-9e12-e2e43121521d

src/language.cpp
@@ -42,6 +42,8 @@
 #include <windows.h>
 #endif
 
+#define DBG_FS LOG_STREAM(debug, filesystem)
+
 /** Tests one locale to be available. */
 static bool has_locale(const char* s) {
 	try {
@@ -355,16 +357,23 @@ void init_textdomains(const config& cfg)
 		if(path.empty()) {
 			t_string::add_textdomain(name, get_intl_dir());
 		} else {
-			const std::string& location = get_binary_file_location(path, "");
-
-			//if location is empty, this causes a crash on Windows, so we
-			//disallow adding empty domains
-			if(location.empty()) {
-				std::cerr << "no location found for '" << path << "', not adding textdomain\n";
-			} else {
-				t_string::add_textdomain(name, location);
+			//This is adapted version from a call to get_binary_file_location()
+			DBG_FS << "  Looking for textdomain path" << path << "\n";
+			const std::vector<std::string>& paths = get_binary_paths(path);
+			for(std::vector<std::string>::const_iterator i = paths.begin(); i != paths.end(); ++i) {
+				DBG_FS << "  Checking " << *i << "\n";
+				const std::string location = *i + path;
+				if(is_directory(location)) {
+					DBG_FS << "  Found at " << location << "\n";
+					t_string::add_textdomain(name, location);
+				} else {
+					//if location is empty, this causes a crash on Windows, so we
+					//disallow adding empty domains
+					std::cerr << "no location found for '" << path << "', not adding textdomain\n";
+				}
 			}
 		}
 	}
+
 }
 

src/filesystem.cpp
@@ -863,7 +863,7 @@ std::string get_binary_file_location(const std::string& type, const std::string&
 	const std::vector<std::string>& paths = get_binary_paths(type);
 	if(!filename.empty()) {
 		DBG_FS << "Looking for " << filename << " in  '.'\n";
-		if(file_exists(filename) || is_directory(filename)) {
+		if(file_exists(filename)) {
 		  DBG_FS << "  Found at " << filename << "\n";
 			return filename;
 		}
@@ -872,7 +872,7 @@ std::string get_binary_file_location(const std::string& type, const std::string&
 	for(std::vector<std::string>::const_iterator i = paths.begin(); i != paths.end(); ++i) {
 		const std::string file = *i + filename;
 		DBG_FS << "  Checking " << *i << "\n";
-		if(file_exists(file) || is_directory(file)) {
+		if(file_exists(file)) {
 		  DBG_FS << "  Found at " << file << "\n";
 			return file;
 		}

commit 299b2d4be8ebeb6e1623514256a48b369c95ac3c

fix incorrect error messages repetition introduced in previous commit (r27176)
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27178 75b3775d-3801-0410-9e12-e2e43121521d

src/language.cpp
@@ -359,19 +359,25 @@ void init_textdomains(const config& cfg)
 		} else {
 			//This is adapted version from a call to get_binary_file_location()
 			DBG_FS << "  Looking for textdomain path" << path << "\n";
+			std::string location;
+
 			const std::vector<std::string>& paths = get_binary_paths(path);
 			for(std::vector<std::string>::const_iterator i = paths.begin(); i != paths.end(); ++i) {
 				DBG_FS << "  Checking " << *i << "\n";
-				const std::string location = *i + path;
-				if(is_directory(location)) {
-					DBG_FS << "  Found at " << location << "\n";
-					t_string::add_textdomain(name, location);
-				} else {
-					//if location is empty, this causes a crash on Windows, so we
-					//disallow adding empty domains
-					std::cerr << "no location found for '" << path << "', not adding textdomain\n";
+				if(is_directory(*i + path)) {
+					location = *i + path;
+					break;
 				}
 			}
+
+			if (location.empty()) {
+				//if location is empty, this causes a crash on Windows, so we
+				//disallow adding empty domains
+				std::cerr << "no location found for '" << path << "', not adding textdomain\n";
+			} else {
+				DBG_FS << "  Found at " << location << "\n";
+				t_string::add_textdomain(name, location);
+			} 
 		}
 	}
 