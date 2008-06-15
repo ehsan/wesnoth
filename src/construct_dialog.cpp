ehsan/wesnoth

commit f73df75493cd7f0ef8535eb4dd1500799705763c

fix bug with filter + delete savegame (Gna! bug #11779)
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27043 75b3775d-3801-0410-9e12-e2e43121521d

src/construct_dialog.cpp
@@ -87,10 +87,10 @@ dialog_textbox::~dialog_textbox()
 }
 
 dialog::dimension_measurements::dimension_measurements() :
-	x(-1), 
-	y(-1), 
+	x(-1),
+	y(-1),
 	interior(empty_rect),
-	message(empty_rect), 
+	message(empty_rect),
 	textbox(empty_rect),
 	menu_width(0),
 	panes(),
@@ -111,8 +111,8 @@ dialog::dimension_measurements::dimension_measurements() :
 }
 
 dialog::dialog(display &disp, const std::string& title, const std::string& message,
-		const DIALOG_TYPE type, const style& dialog_style) : 
-	disp_(disp), 
+		const DIALOG_TYPE type, const style& dialog_style) :
+	disp_(disp),
 	image_(NULL),
 	title_(title),
 	style_(dialog_style),
@@ -850,21 +850,32 @@ void dialog_image::draw_contents()
 	video().blit_surface(location().x, location().y, surf_);
 }
 
-int filter_textbox::get_index(int index) const {
+int filter_textbox::get_index(int selection) const {
 	// don't translate special values
-	if(index < 0) {
-		return index;
+	if(selection < 0) {
+		return selection;
 	}
 	//we must add one to the index to ignore the header row, and
 	//then subtract one from the result to return the index not including
 	//the header row.
-	index++;
-	
-	if (size_t(index) >= index_map_.size()) {
+
+	size_t header = 1; // for now, just assume there's a header row
+
+	if (size_t(selection+header) >= index_map_.size()) {
 		return -1; // bad index, cancel
 	}
 
-	return index_map_[index]-1;
+	return index_map_[selection+header]-header;
+}
+
+void filter_textbox::delete_item(int selection) {
+	size_t header = 1; // for now, just assume there's a header row
+	filtered_items_.erase(filtered_items_.begin() + selection+header);
+	items_.erase(items_.begin() + index_map_[selection+header]);
+	index_map_.erase(index_map_.begin() + selection+header);
+
+	//for now, assume the dialog menu item is deleted using DELETE_ITEM
+	/* dialog_.set_menu_items(filtered_items_); */
 }
 
 void filter_textbox::handle_text_changed(const wide_string& text) {
@@ -880,7 +891,7 @@ void filter_textbox::handle_text_changed(const wide_string& text) {
 			index_map_.push_back(n);
 		}
 	}
-	
+
 	dialog_.set_menu_items(filtered_items_);
 }
 

src/construct_dialog.hpp
@@ -30,17 +30,17 @@ namespace gui {
 struct dialog_process_info
 {
 public:
-	dialog_process_info() : 
+	dialog_process_info() :
 		key(),
-		left_button(true), 
-		right_button(true), 
+		left_button(true),
+		right_button(true),
 		key_down(true),
-		first_time(true), 
+		first_time(true),
 		double_clicked(false),
 		new_left_button(false),
 		new_right_button(false),
 		new_key_down(false),
-		selection(-1), 
+		selection(-1),
 		clear_buttons_(false)
 	{}
 
@@ -132,7 +132,11 @@ public:
 		set_text("");
 	}
 
-	int get_index(int index) const;
+	// current menu selection is based on a possibly filtered view,
+	// and thus may differ from the original, unfiltered index
+	int get_index(int selection) const;
+	void delete_item(int selection);
+
 private:
 	std::vector<std::string> items_, filtered_items_;
 	std::vector<int> index_map_;

src/dialogs.cpp
@@ -340,6 +340,9 @@ gui::dialog_button_action::RESULT delete_save::button_pressed(int menu_selection
 			}
 		}
 
+		// Remove the item from filter_textbox memory
+		filter_.delete_item(menu_selection);
+
 		// Delete the file
 		delete_game(saves_[index].name);
 

commit ab953c8fc8794553321546afd17654eeb17157f0

- Fix a bug introduced by r27043, savegames after a deleted one were shifted (even without using any filter, so bug #11779 was not fixed. It is now) - Fix an inaccuracy with the new filter from the addon manager (always show the first one: the accuracy era) spotted by Shadow_Master.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27185 75b3775d-3801-0410-9e12-e2e43121521d

src/construct_dialog.cpp
@@ -855,24 +855,42 @@ int filter_textbox::get_index(int selection) const {
 	if(selection < 0) {
 		return selection;
 	}
-	//we must add one to the index to ignore the header row, and
-	//then subtract one from the result to return the index not including
-	//the header row.
+	//we must the header row value to the index to ignore this row and
+	//then subtract it from the result to return the index not including
+	//the possible header row.
 
-	size_t header = 1; // for now, just assume there's a header row
-
-	if (size_t(selection+header) >= index_map_.size()) {
+	if (size_t(selection+header_row_) >= index_map_.size()) {
 		return -1; // bad index, cancel
 	}
 
-	return index_map_[selection+header]-header;
+	return index_map_[selection+header_row_]-header_row_;
 }
 
 void filter_textbox::delete_item(int selection) {
-	size_t header = 1; // for now, just assume there's a header row
-	filtered_items_.erase(filtered_items_.begin() + selection+header);
-	items_.erase(items_.begin() + index_map_[selection+header]);
-	index_map_.erase(index_map_.begin() + selection+header);
+	// use the real selection
+	selection += header_row_;
+
+	const size_t size = index_map_.size();
+	assert(size == filtered_items_.size());
+	if (selection >= size)
+		return;
+	int selection_index = index_map_[selection];
+
+	// we erase the selected element by shifting the following others
+	for(size_t i = selection; i < size-1; ++i) {
+		filtered_items_[i] = filtered_items_[i+1];
+		// don't forget to also shift the next index values
+		index_map_[i] = index_map_[i+1] - 1;
+	}
+	// same operation for item, but starting from the index of selection
+	for(size_t i = selection_index; i < items_.size()-1; ++i) {
+		items_[i] = items_[i+1];
+	}
+
+	// finally, we resize to remove the last doubled element
+	filtered_items_.resize(size - 1);
+	index_map_.resize(size - 1);
+	items_.resize(items_.size() - 1);
 
 	//for now, assume the dialog menu item is deleted using DELETE_ITEM
 	/* dialog_.set_menu_items(filtered_items_); */
@@ -883,7 +901,7 @@ void filter_textbox::handle_text_changed(const wide_string& text) {
 	index_map_.clear();
 	const std::string t = utils::wstring_to_string(text);
 	for(size_t n = 0; n != items_.size(); ++n) {
-		if(n == 0 || std::search(items_[n].begin(), items_[n].end(),
+		if(n < header_row_ || std::search(items_[n].begin(), items_[n].end(),
 								 t.begin(), t.end(),
 								 chars_equal_insensitive) != items_[n].end())
 		{

src/construct_dialog.hpp
@@ -122,10 +122,11 @@ class dialog;
 class filter_textbox : public gui::dialog_textbox {
 public:
 	filter_textbox(CVideo& video, const std::string& header,
-		const std::vector<std::string>& items,
+		const std::vector<std::string>& items, size_t header_row,
 		dialog& dialog, int width = 250)
 		: dialog_textbox(new label(video, header), video, width),
 		items_(items),
+		header_row_(header_row),
 		dialog_(dialog),
 		first_time_(true)
 	{
@@ -140,6 +141,7 @@ public:
 private:
 	std::vector<std::string> items_, filtered_items_;
 	std::vector<int> index_map_;
+	size_t header_row_;
 	gui::dialog& dialog_;
 	bool first_time_;
 	virtual void handle_text_changed(const wide_string& text);

src/dialogs.cpp
@@ -664,7 +664,7 @@ std::string load_game_dialog(display& disp, const config& game_config, bool* sho
 	sorter.set_alpha_sort(0).set_id_sort(1);
 	lmenu.set_menu(items, &sorter);
 
-	gui::filter_textbox* filter = new gui::filter_textbox(disp.video(), _("Filter: "), items, lmenu);
+	gui::filter_textbox* filter = new gui::filter_textbox(disp.video(), _("Filter: "), items, 1, lmenu);
 	lmenu.set_textbox(filter);
 
 	save_preview_pane save_preview(disp.video(),game_config,&map_obj,games,summaries,*filter);

src/game.cpp
@@ -1421,7 +1421,7 @@ void game_controller::download_addons(std::string host)
 		addon_dialog.set_menu(addon_menu);
 
 		gui::filter_textbox* filter = new gui::filter_textbox(disp().video(),
-			_("Filter: "), options, addon_dialog, 300);
+			_("Filter: "), options, 0, addon_dialog, 300);
 		addon_dialog.set_textbox(filter);
 
 		const int index = addon_dialog.show();

src/menu_events.cpp
@@ -1660,7 +1660,7 @@ private:
 			umenu.set_menu(options, &sorter);
 
 			gui::filter_textbox* filter = new gui::filter_textbox(gui_->video(),
-				_("Filter: "), options, umenu, 200);
+				_("Filter: "), options, 1, umenu, 200);
 			umenu.set_textbox(filter);
 
 			//sort by race then by type name