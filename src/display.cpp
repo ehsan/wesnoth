ehsan/wesnoth

commit 0fb88652ef5ad080198a5d271551b06c6a7cb5bb

display::draw() refactoring -- mostly done, needs proofreading, testing, some cleanup and comments. Commiting so others can look at it. This appears to have uncovered a spurious crash related to display::scroll that needs fixing.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27280 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -22,6 +22,7 @@
 #include "display.hpp"
 #include "events.hpp"
 #include "font.hpp"
+#include "foreach.hpp"
 #include "game_config.hpp"
 #include "gettext.hpp"
 #include "hotkeys.hpp"
@@ -92,6 +93,9 @@ display::display(CVideo& video, const gamemap& map, const config& theme_cfg, con
 	turbo_(false),
 	invalidateGameStatus_(true),
 	map_labels_(*this,map, 0),
+	shroud_image_("terrain/" + map_.get_terrain_info(t_translation::VOID_TERRAIN).minimap_image() + ".png"),
+	fog_image_("terrain/" + map_.get_terrain_info(t_translation::FOGGED).minimap_image() + ".png"),
+	tod_(time_of_day()),
 	_scroll_event("scrolled"),
 	nextDraw_(0),
 	report_(),
@@ -1412,8 +1416,14 @@ void display::scroll(int xmove, int ymove)
 	SDL_Rect srcrect = dstrect;
 	srcrect.x -= dx;
 	srcrect.y -= dy;
-
-	SDL_BlitSurface(screen,&srcrect,screen,&dstrect);
+	DBG_DP << "xmove is " << xmove << ", ymove is " << ymove << "\n";
+	DBG_DP << "screen is: w" << screen->w << " h" << screen->h << "\n";	
+	DBG_DP << "map_area() is: x" << map_area().x << " y" << map_area().y << " w" << map_area().w << " h" << map_area().h << "\n";
+	DBG_DP << "srcrect is: x" << srcrect.x << " y" << srcrect.y << " w" << srcrect.w << " h" << srcrect.h << "\n";
+	DBG_DP << "dstrect is: x" << dstrect.x << " y" << dstrect.y << " w" << dstrect.w << " h" << dstrect.h << "\n";
+	if (!screen_.update_locked())
+		SDL_BlitSurface(screen,&srcrect,screen,&dstrect);
+	DBG_DP << "blitted\n";
 
 	// Invalidate locations in the newly visible rects
 
@@ -1431,7 +1441,6 @@ void display::scroll(int xmove, int ymove)
 		r.w = abs(dx);
 		invalidate_locations_in_rect(r);
 	}
-
 	_scroll_event.notify_observers();
 	update_rect(map_area());
 
@@ -1490,7 +1499,6 @@ void display::scroll_to_xy(int screenxpos, int screenypos, SCROLL_TYPE scroll_ty
 	if(screen_.update_locked()) {
 		return;
 	}
-
 	const SDL_Rect area = map_area();
 	const int xmove_expected = screenxpos - (area.x + area.w/2);
 	const int ymove_expected = screenypos - (area.y + area.h/2);
@@ -1632,7 +1640,7 @@ void display::scroll_to_tiles(const std::vector<gamemap::location>& locs,
 			miny = miny_new;
 			maxx = maxx_new;
 			maxy = maxy_new;
-			
+
 		}
 	}
 	//if everything is fogged or the locs list is empty
@@ -1821,10 +1829,156 @@ void display::redraw_everything()
 	map_labels_.recalculate_labels();
 
 	redraw_background_ = true;
-
+	
+	int ticks1 = SDL_GetTicks();
 	invalidate_all();
+	int ticks2 = SDL_GetTicks();
 	draw(true,true);
+	int ticks3 = SDL_GetTicks();
+	INFO_DP << "invalidate and draw: " << (ticks3 - ticks2) << " and " << (ticks2 - ticks1) << "\n";
 }
+
+void display::draw(bool update,bool force) {
+//	log_scope("display::draw");
+	if (screen_.update_locked()) {
+		return;
+	}
+	bool changed = draw_init();
+	// invalidate all that needs to be invalidated
+	invalidate_animations();
+	pre_draw();
+	update_time_of_day();
+	if(!map_.empty()) {
+		halo::unrender(invalidated_);
+		//int simulate_delay = 0;
+		if(!invalidated_.empty()) {
+			changed = true;
+			INFO_DP << invalidated_.size() << "\n";
+			draw_invalidated();	
+			invalidated_.clear();
+		}
+		drawing_buffer_commit();
+		halo::render();
+		draw_sidebar();
+		//! @todo FIXME: This changed can probably be smarter
+		changed = true;
+		// Simulate slow PC:
+		//SDL_Delay(2*simulate_delay + rand() % 20);
+	}
+	draw_wrap(update, force, changed);
+}
+
+void display::clear_screen()
+{
+	surface const disp(screen_.getSurface());
+	SDL_Rect area = screen_area();
+	SDL_FillRect(disp, &area, SDL_MapRGB(disp->format, 0, 0, 0));	
+}
+
+void display::pre_draw()
+{
+	//no action by default		
+}
+
+const SDL_Rect& display::get_clip_rect()
+{
+	return map_area();
+}
+
+void display::draw_invalidated() {
+//	log_scope("display::draw_invalidated");
+	SDL_Rect clip_rect = get_clip_rect();
+	surface screen = get_screen_surface();
+	clip_rect_setter set_clip_rect(screen, clip_rect);
+	foreach (gamemap::location loc, invalidated_) {
+		int xpos = get_location_x(loc);
+		int ypos = get_location_y(loc);
+		const bool on_map = map_.on_board(loc);
+		const bool off_map_tile = (map_.get_terrain(loc) == t_translation::OFF_MAP_USER);
+		SDL_Rect hex_rect = {xpos, ypos, zoom_, zoom_};
+		if(!rects_overlap(hex_rect,clip_rect)) {
+			continue;
+		}			
+		draw_hex(loc);
+		drawing_buffer_commit();
+		// If the tile is at the border, we start to blend it
+		if(!on_map && !off_map_tile) {
+			 draw_border(loc, xpos, ypos);
+		}			
+	}		
+}
+
+void display::draw_hex(const gamemap::location& loc) {
+	int xpos = get_location_x(loc);
+	int ypos = get_location_y(loc);
+	int drawing_order = gamemap::get_drawing_order(loc);
+	image::TYPE image_type = get_image_type(loc);
+	const bool on_map = map_.on_board(loc);	
+	const bool off_map_tile = (map_.get_terrain(loc) == t_translation::OFF_MAP_USER);
+	if(!shrouded(loc)) {
+		// unshrouded terrain (the normal case)
+		drawing_buffer_add(LAYER_TERRAIN_BG, drawing_order, tblit(xpos, ypos,
+			get_terrain_images(loc,tod_.id, image_type, ADJACENT_BACKGROUND)));
+
+		//FIXME: Use a hack to draw terrain in the unit layer
+		// but with a higher drawing_order, so it's rendered on top of the unit
+		drawing_buffer_add(LAYER_UNIT_FIRST, drawing_order+100, tblit(xpos, ypos,
+			get_terrain_images(loc,tod_.id,image_type,ADJACENT_FOREGROUND)));
+		// drawing_buffer_add(LAYER_TERRAIN_FG, drawing_order, tblit(xpos, ypos,
+		//	get_terrain_images(*it,tod.id,image_type,ADJACENT_FOREGROUND)));
+				
+	// Draw the grid, if that's been enabled
+		if(grid_ && on_map && !off_map_tile) {
+			drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos,
+				image::get_image(game_config::grid_image, image::SCALED_TO_HEX)));
+		}
+	}
+
+
+	// Add the top layer overlay surfaces
+	if(!hex_overlay_.empty()) {
+		std::map<gamemap::location, surface>::const_iterator itor = hex_overlay_.find(loc);
+		if(itor != hex_overlay_.end())
+			drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos, itor->second));
+	}
+
+	// Paint selection and mouseover overlays
+	if(loc == selectedHex_ && on_map && selected_hex_overlay_ != NULL) {
+		drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos, selected_hex_overlay_));
+	}
+	if(loc == mouseoverHex_ && on_map && mouseover_hex_overlay_ != NULL) {
+		drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos, mouseover_hex_overlay_));
+	}
+	
+	// Apply shroud, fog and linger overlay
+	if(shrouded(loc)) {
+		// We apply void also on off-map tiles
+		// to shroud the half-hexes too
+		drawing_buffer_add(LAYER_FOG_SHROUD, drawing_order, tblit(xpos, ypos,
+			image::get_image(shroud_image_, image_type)));
+	} else if(fogged(loc)) {
+		drawing_buffer_add(LAYER_FOG_SHROUD, drawing_order, tblit(xpos, ypos,
+			image::get_image(fog_image_, image_type)));
+	}
+
+	if(!shrouded(loc)) {
+		drawing_buffer_add(LAYER_FOG_SHROUD, drawing_order, tblit(xpos, ypos,
+			get_terrain_images(loc, tod_.id, image_type, ADJACENT_FOGSHROUD)));
+	}
+}
+
+image::TYPE display::get_image_type(const gamemap::location& loc) {
+	return image::SCALED_TO_HEX;
+}
+
+void display::update_time_of_day() {
+	//no action
+}
+
+void display::draw_sidebar() {
+	
+}
+
 
 void display::draw_image_for_report(surface& img, SDL_Rect& rect)
 {
@@ -2071,4 +2225,24 @@ bool display::rectangle_need_update(const gamemap::location& first_corner, const
 bool display::zone_need_update(const int x1,const int y1, const int x2, const int y2) const {
 	const SDL_Rect& rect = map_area();
 	return rectangle_need_update(pixel_position_to_hex(x1 - rect.x+xpos_, y1 - rect.y+ypos_),pixel_position_to_hex(x2 - rect.x+xpos_, y2 - rect.y+ypos_));
+}
+
+void display::invalidate_animations() {
+	if (preferences::animate_map()) {
+		gamemap::location topleft;
+		gamemap::location bottomright;
+		get_visible_hex_bounds(topleft, bottomright);
+		for(int x = topleft.x; x <= bottomright.x; ++x) {
+			for(int y = topleft.y; y <= bottomright.y; ++y) {
+				const gamemap::location loc(x,y);
+				if (!shrouded(loc)) {
+					if (builder_.update_animation(loc)) {
+						invalidate(loc);
+					} else {
+						invalidate_animations_location(loc);
+					}
+				}
+			}
+		}
+	}
 }

src/display.hpp
@@ -26,11 +26,13 @@ class unit_map;
 
 #include "builder.hpp"
 #include "generic_event.hpp"
+#include "halo.hpp"
 #include "image.hpp"
 #include "key.hpp"
 #include "map.hpp"
 #include "map_label.hpp"
 #include "reports.hpp"
+#include "time_of_day.hpp"
 #include "theme.hpp"
 #include "video.hpp"
 #include "widgets/button.hpp"
@@ -157,11 +159,13 @@ public:
 	int get_location_y(const gamemap::location& loc) const;
 
 	//! Returns true if location (x,y) is covered in shroud.
-	bool shrouded(const gamemap::location& loc) const
-		{return viewpoint_ && viewpoint_->shrouded(loc);}
+	bool shrouded(const gamemap::location& loc) const {
+		return viewpoint_ && viewpoint_->shrouded(loc);
+	}
 	//! Returns true if location (x,y) is covered in fog.
-	bool fogged(const gamemap::location& loc) const
-		{return viewpoint_ && viewpoint_->fogged(loc);}
+	bool fogged(const gamemap::location& loc) const { 
+		return viewpoint_ && viewpoint_->fogged(loc);
+	}
 
 	//! Determines whether a grid should be overlayed on the game board.
 	//! (to more clearly show where hexes are)
@@ -189,10 +193,16 @@ public:
 	// Will be overridden in the display subclass
 	virtual bool invalidate(const gamemap::location& loc) {return invalidated_.insert(loc).second;};
 	virtual bool invalidate_rectangle(const gamemap::location& first_corner, const gamemap::location& second_corner) ;
-	virtual bool invalidate_zone(const int x1,const int y1, const int x2, const int y2); 
+	virtual bool invalidate_zone(const int x1,const int y1, const int x2, const int y2);
 	virtual bool rectangle_need_update(const gamemap::location& first_corner, const gamemap::location& second_corner) const;
-	virtual bool zone_need_update(const int x1,const int y1, const int x2, const int y2) const; 
+	virtual bool zone_need_update(const int x1,const int y1, const int x2, const int y2) const;
 	virtual void draw_minimap_units() {};
+	virtual void invalidate_animations();
+	/**
+	 * Per-location invalidation called by invalidate_animations()
+	 * defaults to no action, overriden by derived classes
+	 */
+	virtual void invalidate_animations_location(gamemap::location loc){}
 
 	const gamemap& get_map()const { return map_;}
 
@@ -328,10 +338,14 @@ public:
 	//! Check if a tile is fully on screen.
 	bool tile_on_screen(const gamemap::location& loc);
 
-	//! Draws invalidated items.
-	//! If update is true, will also copy the display to the frame buffer.
-	//! If force is true, will not skip frames, even if running behind.
-	virtual void draw(bool update=true,bool force=false) = 0;
+	/** 
+	 * Draws invalidated items.
+	 * If update is true, will also copy the display to the frame buffer.
+	 * If force is true, will not skip frames, even if running behind.
+	 * Not virtual, since it gathers common actions. Calls various protected
+	 * virtuals (further below) to allow specialized behaviour in derived classes.
+	 */
+	void draw(bool update=true, bool force=false);
 
 	map_labels& labels() { return map_labels_; }
 	const map_labels& labels() const { return map_labels_; }
@@ -357,7 +371,52 @@ public:
 	std::map<reports::TYPE, std::string> get_report_contents() {return report_;};
 
 protected:
+	/** Clear the screen contents */
+	void clear_screen();
 
+	/**
+	 * Called near the beginning of each draw() call.
+	 * Derived classes can use this to add extra actions before redrawing
+	 * invalidated hexes takes place
+	 */
+	virtual void pre_draw();
+	
+	/**
+	 * Get the clipping rectangle for drawing.
+	 * Virtual since the editor might use a slightly different approach.
+	 */
+	virtual const SDL_Rect& get_clip_rect();
+	
+	/**
+	 * Only called when there's actual redrawing to do. Loops through
+	 * invalidated locations and redraws them. Derived classes can override
+	 * this, possibly to insert pre- or post-processing around a call to the
+	 * base class's function.
+	 */
+	virtual void draw_invalidated();
+	
+	/**
+	 * Redraws a single gamemap location.
+	 */
+	virtual void draw_hex(const gamemap::location& loc);
+	
+	/**
+	 * @returns the image type to be used for the passed hex
+	 * (mostly to do with brightening like for mouseover)
+	 */
+	virtual image::TYPE get_image_type(const gamemap::location& loc);
+
+	/**
+	 * Update time of day member to the tod to be used in the current drawing
+	 */
+	virtual void update_time_of_day();
+	
+	/**
+	 * Called near the end of a draw operation, derived classes can use this 
+	 * to render a specific sidebar
+	 */
+	virtual void draw_sidebar();
+	
 	/**
 	 * Draws the border tile overlay.
 	 * The routine determines by itself which border it is on
@@ -406,6 +465,10 @@ protected:
 	bool turbo_;
 	bool invalidateGameStatus_;
 	map_labels map_labels_;
+	std::string shroud_image_;
+	std::string fog_image_;
+	time_of_day tod_;
+
 	//! Event raised when the map is being scrolled
 	mutable events::generic_event _scroll_event;
 	//! Holds the tick count for when the next drawing event is scheduled.
@@ -427,7 +490,7 @@ protected:
 	std::set<gamemap::location> highlighted_locations_;
 	CKey keys_;
 
-public:	
+public:
 	//! Helper structure for rendering the terrains.
 	struct tblit{
 		tblit(const int x, const int y) :
@@ -437,7 +500,7 @@ public:
 			clip()
 			{}
 
-		tblit(const int x, const int y, const surface& surf, 
+		tblit(const int x, const int y, const surface& surf,
 				const SDL_Rect& clip = SDL_Rect()) :
 			x(x),
 			y(y),
@@ -445,7 +508,7 @@ public:
 			clip(clip)
 			{}
 
-		tblit(const int x, const int y, const std::vector<surface>& surf, 
+		tblit(const int x, const int y, const std::vector<surface>& surf,
 				const SDL_Rect& clip = SDL_Rect()) :
 			x(x),
 			y(y),
@@ -463,17 +526,17 @@ public:
 
 	//! The layers to render something on. This value should never be stored
 	//! it's the internal drawing order and adding removing and reordering
-	//! the layers should be save.
+	//! the layers should be safe.
 	//! If needed in WML use the name and map that to the enum value.
-	enum tdrawing_layer{ 
-		LAYER_TERRAIN_BG,          //!< Layer for the terrain drawn behind the 
+	enum tdrawing_layer{
+		LAYER_TERRAIN_BG,          //!< Layer for the terrain drawn behind the
 		                           //!< unit.
 		LAYER_TERRAIN_TMP_BG,      //!< Layer which holds stuff that needs to be
 		                           //!< sorted out further, but under units
 		LAYER_UNIT_BG,             //!< Used for the ellipse behind the unit.
 		LAYER_UNIT_FIRST,          //!< Reserve layeres to be selected for WML.
 		LAYER_UNIT_LAST=LAYER_UNIT_FIRST+100,
-		LAYER_UNIT_FG,             //!< Used for the ellipse in front of the 
+		LAYER_UNIT_FG,             //!< Used for the ellipse in front of the
 		                           //!< unit.
 		LAYER_UNIT_FAKE,           //!< The fake unit is drawn on this layer.
 		LAYER_TERRAIN_FG,          //!< Layer for the terrain drawn in front of
@@ -486,7 +549,7 @@ public:
 		                           //!< this layer (for testing here).
 		LAYER_MOVE_INFO,           //!< Movement info (defense%, ect...)
 		LAYER_LINGER_OVERLAY,      //!< The overlay used for the linger mode.
-		
+
 		LAYER_LAST_LAYER           //!< Don't draw to this layer it's a dummy
 		                           //! to size the vector.
 		};
@@ -494,7 +557,7 @@ public:
 	//! Draw text on a hex. (0.5, 0.5) is the center.
 	//! The font size is adjusted to the zoom factor
 	//! and divided by 2 for tiny-gui.
-	void draw_text_in_hex(const gamemap::location& loc, 
+	void draw_text_in_hex(const gamemap::location& loc,
 		const tdrawing_layer layer, const std::string& text, size_t font_size,
 		SDL_Color color, double x_in_hex=0.5, double y_in_hex=0.5);
 
@@ -503,16 +566,16 @@ protected:
 	// Initially tdrawing_buffer was a vector but profiling showed that a map
 	// was more efficient. Tested with the LAYER_UNIT_LAST for various values
 	// and different types the results were. (Tested with oprofile.)
-  
+
  	// container    unit layers    counts
 	// vector       100            3748
 	// vector       10000          147338
 	// map          10000          3362
 
 	// Since a map with 10000 items was more efficient I didn't test the map
-	// with 100 items. I want to retest once more is converted, since with 
+	// with 100 items. I want to retest once more is converted, since with
 	// a different usage it numbers might differ so the old code is disabled
-	// with TDRAWING_BUFFER_USES_VECTOR 
+	// with TDRAWING_BUFFER_USES_VECTOR
 	//     20080308 -- Mordante
 
 	//! * Surfaces are rendered per level in a map.
@@ -523,9 +586,9 @@ protected:
 	//! * every vector element has a vector with surfaces to render.
 #if TDRAWING_BUFFER_USES_VECTOR
 	typedef std::vector<std::map<int /*drawing_order*/, std::vector<tblit> > > tdrawing_buffer;
-#else	
+#else
 	typedef std::map<tdrawing_layer, std::map<int /*drawing_order*/, std::vector<tblit> > > tdrawing_buffer;
-#endif	
+#endif
 	tdrawing_buffer drawing_buffer_;
 
 public:
@@ -577,30 +640,6 @@ private:
 	double idle_anim_rate_;
 
 	surface map_screenshot_surf_;
-};
-
-//! Simplified display class for the editor.
-//! It only needs to draw terrain, no units, no fog, etc.
-class editor_display : public display
-{
-public:
-	editor_display(CVideo& video, const gamemap& map, const config& theme_cfg,
-			const config& cfg, const config& level);
-
-	bool in_editor() const { return true; }
-
-	//! draw() for the editor display.
-	//! It only has to know about terrain.
-	void draw(bool update=true,bool force=false);
-
-	//! Rebuild the dynamic terrain at the given location.
-	void rebuild_terrain(const gamemap::location &loc)
-		{ builder_.rebuild_terrain(loc); }
-
-	//! Updates editor light levels from preferences
-	void update_light_levels();
-private:
-	int lr_, lg_, lb_;
 };
 
 #endif

src/editor/editor.hpp
@@ -22,6 +22,7 @@
 
 #include "SDL.h"
 
+#include "editor_display.hpp"
 #include "editor_palettes.hpp"
 #include "editor_layout.hpp"
 #include "editor_undo.hpp"

src/editor/editor_display.cpp
@@ -17,7 +17,7 @@
 
 #include "global.hpp"
 
-#include "display.hpp"
+#include "editor_display.hpp"
 #include "game_config.hpp"
 #include "gamestatus.hpp"
 #include "gettext.hpp"
@@ -38,89 +38,34 @@ editor_display::editor_display(CVideo& video, const gamemap& map,
 		const config& level) :
 	display(video, map, theme_cfg, cfg, level)
 {
-	// Clear the screen contents
-	surface const disp(screen_.getSurface());
-	SDL_Rect area = screen_area();
-	SDL_FillRect(disp,&area,SDL_MapRGB(disp->format,0,0,0));
+    clear_screen();
 }
 
-void editor_display::draw(bool update,bool force)
+void editor_display::pre_draw()
 {
-	bool changed = display::draw_init();
-
 	//!@todo FIXME: this should be done from the preferences dialog as well,
 	//! to reflect changes for the user before closing it
 	this->update_light_levels();
 	image::set_colour_adjustment(lr_, lg_, lb_);
+}
 
-	//int simulate_delay = 0;
-	if(!map_.empty() && !invalidated_.empty()) {
-		changed = true;
+image::TYPE editor_display::get_image_type(const gamemap::location& loc)
+{
+    if(loc == mouseoverHex_ && map_.on_board_with_border(mouseoverHex_)) {
+        return image::BRIGHTENED;
+    } else if (highlighted_locations_.find(loc) != highlighted_locations_.end()) {
+        return image::SEMI_BRIGHTENED;
+    }
+    return image::SCALED_TO_HEX;
+}
 
-		SDL_Rect clip_rect = map_outside_area();
-		surface const screen(get_screen_surface());
-		clip_rect_setter set_clip_rect(screen, clip_rect);
+const SDL_Rect& editor_display::get_clip_rect()
+{
+    return map_outside_area();
+}
 
-		std::set<gamemap::location>::const_iterator it;
-		for(it = invalidated_.begin(); it != invalidated_.end(); ++it) {
-			image::TYPE image_type = image::SCALED_TO_HEX;
-
-			if(*it == mouseoverHex_ && map_.on_board_with_border(mouseoverHex_)) {
-				image_type = image::BRIGHTENED;
-			}
-			else if (highlighted_locations_.find(*it) != highlighted_locations_.end()) {
-				image_type = image::SEMI_BRIGHTENED;
-			}
-
-			if(screen_.update_locked()) {
-				continue;
-			}
-
-			int xpos = int(get_location_x(*it));
-			int ypos = int(get_location_y(*it));
-			int drawing_order = gamemap::get_drawing_order(*it);
-
-			if(xpos >= clip_rect.x + clip_rect.w || ypos >= clip_rect.y + clip_rect.h ||
-			   xpos + zoom_ < clip_rect.x || ypos + zoom_ < clip_rect.y) {
-				continue;
-			}
-
-			//!@todo FIXME: this seems to have no effect, and to be left-over of the
-			//! time of day light system used before around version 0.9
-			const std::string nodarken = "morning";
-			drawing_buffer_add(LAYER_TERRAIN_BG, drawing_order, tblit(xpos, ypos,
-				get_terrain_images(*it,nodarken,image_type,ADJACENT_BACKGROUND)));
-			drawing_buffer_add(LAYER_TERRAIN_FG, drawing_order, tblit(xpos, ypos,
-				get_terrain_images(*it,nodarken,image_type,ADJACENT_FOREGROUND)));
-
-			// Draw the grid, if it has been enabled
-			if(grid_ && map_.on_board(*it)) {
-				drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos,
-					image::get_image(game_config::grid_image, image::SCALED_TO_HEX)));
-			}
-
-			// Paint selection and mouseover overlays
-			if(*it == selectedHex_ && map_.on_board_with_border(selectedHex_) && selected_hex_overlay_ != NULL) {
-				drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos, selected_hex_overlay_));
-			}
-			if(*it == mouseoverHex_ && map_.on_board_with_border(mouseoverHex_) && mouseover_hex_overlay_ != NULL) {
-				drawing_buffer_add(LAYER_TERRAIN_TMP_BG, drawing_order, tblit(xpos, ypos, mouseover_hex_overlay_));
-			}
-
-			drawing_buffer_commit();
-
-			// If the tile is at the border, we start to blend it
-			if(!map_.on_board(*it) &&
-					(map_.get_terrain(*it) != t_translation::OFF_MAP_USER)) { 
-				 draw_border(*it, xpos, ypos);
-			}
-		}
-
-		invalidated_.clear();
-	} else if (!map_.empty()) {
-		assert(invalidated_.empty());
-	}
-
+void editor_display::draw_sidebar()
+{
 	// Fill in the terrain report
 	if(map_.on_board_with_border(mouseoverHex_)) {
 		const t_translation::t_terrain terrain = map_.get_terrain(mouseoverHex_);
@@ -147,14 +92,7 @@ void editor_display::draw(bool update,bool force)
 		str2 << mouseoverHex_;
 		refresh_report(reports::POSITION, reports::report(str2.str()));
 	}
-
-	{
-	  std::stringstream str3;
-	  str3 << map_.villages().size();
-	  refresh_report(reports::VILLAGES, reports::report(str3.str()));
-	}
-
-	display::draw_wrap(update, force, changed);
+	refresh_report(reports::VILLAGES, reports::report(lexical_cast<std::string>(map_.villages().size())));
 }
 
 void editor_display::update_light_levels(void)
@@ -163,4 +101,3 @@ void editor_display::update_light_levels(void)
 	this->lg_ = preferences::editor_g();
 	this->lb_ = preferences::editor_b();
 }
-

src/editor/editor_display.hpp
@@ -1,0 +1,46 @@
+/* $Id$ */
+/*
+   Copyright (C) 2003 - 2008 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+//** @file editor_display.hpp */
+
+#ifndef EDITOR_DISPLAY_H_INCLUDED
+#define EDITOR_DISPLAY_H_INCLUDED
+
+#include "../display.hpp"
+class editor_display : public display
+{
+public:
+	editor_display(CVideo& video, const gamemap& map, const config& theme_cfg,
+			const config& cfg, const config& level);
+
+	bool in_editor() const { return true; }
+
+	/** Rebuild the dynamic terrain at the given location. */
+	void rebuild_terrain(const gamemap::location &loc) {
+        builder_.rebuild_terrain(loc);
+    }
+protected:
+    void pre_draw();
+    image::TYPE get_image_type(const gamemap::location& loc);
+    const SDL_Rect& get_clip_rect();
+    void draw_sidebar();
+    
+
+	/** Updates editor light levels from preferences */
+	void update_light_levels();
+private:
+	int lr_, lg_, lb_;
+};
+#endif
+

src/game_display.hpp
@@ -60,9 +60,6 @@ public:
 	//! This will normally be the playing team.
 	void scroll_to_leader(unit_map& units, int side, SCROLL_TYPE scroll_type = ONSCREEN);
 
-	//! Draw for the game display has to know about units
-	void draw(bool update=true,bool force=false);
-
 	//! Function to display a location as selected.
 	//! If a unit is in the location, and there is no unit
 	//! in the currently highlighted hex,
@@ -118,10 +115,35 @@ public:
 	//! Same as invalidate_unit() if moving the displayed unit.
 	void invalidate_unit_after_move(const gamemap::location& src, const gamemap::location& dst);
 
-private:
+protected:
+	/**
+	 * game_display pre_draw does specific things related e.g. to unit rendering
+	 */
+	void pre_draw();
+	
+	/**
+	 * This function runs through invalidated_ and returns a vector of tiles
+	 * containing units, sorted according to a custom ordering
+	 */
+	std::vector<gamemap::location> get_invalidated_unit_locations();
+	
+	image::TYPE get_image_type(const gamemap::location& loc);
+	
+	/**
+	 * Draws units on specified hexes
+	 */
+	void redraw_units(const std::vector<gamemap::location>& invalidated_unit_locations);
+	
+	void draw_invalidated();
+	
+	void draw_hex(const gamemap::location& loc);
+	
+	void update_time_of_day();
+	
+
 	//! Function to invalidate animated terrains which may have changed.
 	void invalidate_animations();
-
+	void invalidate_animations_location(gamemap::location loc);
 	virtual void draw_minimap_units();
 
 public:
@@ -133,9 +155,10 @@ public:
 	void set_attack_indicator(const gamemap::location& src, const gamemap::location& dst);
 	void clear_attack_indicator();
 	//! Function to get attack direction suffix
-	const std::string attack_indicator_direction() const
-	{ return gamemap::location::write_direction(
-		attack_indicator_src_.get_relative_dir(attack_indicator_dst_)); }
+	const std::string attack_indicator_direction() const { 
+		return gamemap::location::write_direction(
+			attack_indicator_src_.get_relative_dir(attack_indicator_dst_));
+	}
 
 	//! Functions to add and remove overlays from locations.
 	//! An overlay is an image that is displayed on top of the tile.
@@ -191,7 +214,7 @@ public:
 	void begin_game();
 
 	virtual bool in_game() const { return in_game_; }
-	void draw_bar(const std::string& image, int xpos, int ypos, 
+	void draw_bar(const std::string& image, int xpos, int ypos,
 		const int drawing_order, size_t height, double filled, const SDL_Color& col, fixed_t alpha);
 
 	//! Sets the linger mode for the display.
@@ -199,9 +222,9 @@ public:
 	//! the extra variables make it easier to modify the behaviour. There
 	//! might even be a split between victory and defeat.
 	//
-	//! @todo if the current implementation is wanted we can change 
+	//! @todo if the current implementation is wanted we can change
 	//! the stuff back to a boolean
-	enum tgame_mode { 
+	enum tgame_mode {
 		RUNNING,         //!< no linger overlay, show fog and shroud
 		LINGER_SP,       //!< linger overlay, show fog and shroud
 		LINGER_MP };     //!< linger overlay, show fog and shroud

commit afaf880e74eca68af31b465f3d5cac30fa0613d3

Fix rendering order of foreground terrain (broken by r27280)
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27337 75b3775d-3801-0410-9e12-e2e43121521d

src/display.cpp
@@ -1863,8 +1863,7 @@ void display::draw_invalidated() {
 		if(!on_map && !off_map_tile) {
 			 draw_border(loc, xpos, ypos);
 		}			
-		drawing_buffer_commit();
-	}		
+	}
 }
 
 void display::draw_hex(const gamemap::location& loc) {