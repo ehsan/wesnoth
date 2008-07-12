ehsan/wesnoth

commit f6ad39c4b09a63a48594c880d07e40413d9cacdc

Lazy loading for unit_types Step 1: Implement the basic logic for lazy loading. The goal of this step is to provide the functionality with keeping impact on the rest of the application as low as possible. Further optimizations will follow.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@25212 75b3775d-3801-0410-9e12-e2e43121521d

src/help.cpp
@@ -52,6 +52,9 @@
 #include <set>
 #include <sstream>
 
+#define DBG_HELP LOG_STREAM(debug, help)
+#define LOG_HELP LOG_STREAM(info, help)
+#define ERR_HELP LOG_STREAM(err, help)
 
 namespace help {
 
@@ -132,7 +135,9 @@ public:
 	explicit topic_text(topic_generator *g): generator_(g) {}
 	topic_text &operator=(topic_generator *g);
 	topic_text(topic_text const &t);
-	operator std::vector< std::string > const &() const;
+	//operator std::vector< std::string > const &() const;
+
+    const std::vector<std::string>& parsed_text() const;
 };
 
 /// A topic contains a title, an id and some text.
@@ -151,7 +156,7 @@ struct topic
 	/// Comparison on the ID.
 	bool operator<(const topic &) const;
 	std::string title, id;
-	topic_text text;
+	mutable topic_text text;
 };
 
 typedef std::list<topic> topic_list;
@@ -853,6 +858,9 @@ bool topic_is_referenced(const std::string &topic_id, const config &cfg)
 void parse_config_internal(const config *help_cfg, const config *section_cfg,
 						   section &sec, int level)
 {
+    if (section_cfg != NULL)
+        DBG_HELP << "parse_config_internal, section_cfg:\n" << *section_cfg;
+
 	if (level > max_section_level) {
 		std::cerr << "Maximum section depth has been reached. Maybe circular dependency?"
 				  << std::endl;
@@ -1036,7 +1044,7 @@ topic_text &topic_text::operator=(topic_generator *g)
 	return *this;
 }
 
-topic_text::operator std::vector< std::string > const &() const
+const std::vector<std::string>& topic_text::parsed_text() const
 {
 	if (generator_) {
 		parsed_text_ = parse_text((*generator_)());
@@ -1128,6 +1136,7 @@ std::vector<topic> generate_ability_topics(const bool sort_generated)
 	if (game_info == NULL) {
 		return topics;
 	}
+
 	std::map<std::string, std::string> ability_description;
 	std::map<std::string, std::set<std::string> > ability_units;
 	// Look through all the unit types, check if a unit of this type
@@ -1297,8 +1306,8 @@ public:
 		// respective topic.
 		const std::string race_id = type_.race();
 		std::string race_name;
-		const race_map::const_iterator race_it = game_info->races.find(race_id);
-		if (race_it != game_info->races.end()) {
+		const race_map::const_iterator race_it = game_info->unit_types.races().find(race_id);
+		if (race_it != game_info->unit_types.races().end()) {
 			race_name = race_it->second.plural_name();
 		} else {
 			race_name = _ ("race^Miscellaneous");
@@ -1546,12 +1555,12 @@ void generate_races_sections(const config *help_cfg, section &sec, int level)
 		config section_cfg;
 
 		bool hidden = (visible_races.count(*it) == 0);
-		
+
 		section_cfg["id"] = hidden_symbol(hidden) + race_prefix + *it;
-		
+
 		std::string title;
-		const race_map::const_iterator race_it = game_info->races.find(*it);
-		if (race_it != game_info->races.end()) {
+		const race_map::const_iterator race_it = game_info->unit_types.races().find(*it);
+		if (race_it != game_info->unit_types.races().end()) {
 			title = race_it->second.plural_name();
 		} else {
 			title = _ ("race^Miscellaneous");
@@ -1574,7 +1583,7 @@ std::vector<topic> generate_unit_topics(const bool sort_generated, const std::st
 	}
 
 	std::set<std::string> race_units;
-	
+
 	for(game_data::unit_type_map::const_iterator i = game_info->unit_types.begin();
 	    i != game_info->unit_types.end(); i++) {
 		const unit_type &type = (*i).second;
@@ -1588,7 +1597,7 @@ std::vector<topic> generate_unit_topics(const bool sort_generated, const std::st
 		const std::string lang_name = type.language_name();
 		const std::string ref_id = hidden_symbol(type.hide_help()) + unit_prefix +  type.id();
 		topic unit_topic(lang_name, ref_id, "");
-		unit_topic.text = new unit_topic_generator(type);
+		//unit_topic.text = new unit_topic_generator(type);
 		topics.push_back(unit_topic);
 
 		if (!type.hide_help()) {
@@ -1603,8 +1612,8 @@ std::vector<topic> generate_unit_topics(const bool sort_generated, const std::st
 	std::string race_id = "..race_"+race;
 	std::string race_name;
 	std::string race_description;
-	const race_map::const_iterator race_it = game_info->races.find(race);
-	if (race_it != game_info->races.end()) {
+	const race_map::const_iterator race_it = game_info->unit_types.races().find(race);
+	if (race_it != game_info->unit_types.races().end()) {
 		race_name = race_it->second.plural_name();
 		race_description = race_it->second.description();
 		// if (description.empty()) description =  _("No description Available");
@@ -2034,7 +2043,7 @@ void help_text_area::set_items()
 		down_one_line();
 	}
 	// Parse and add the text.
-	std::vector<std::string> const &parsed_items = shown_topic_->text;
+	std::vector<std::string> const &parsed_items = shown_topic_->text.parsed_text();
 	std::vector<std::string>::const_iterator it;
 	for (it = parsed_items.begin(); it != parsed_items.end(); it++) {
 		if (*it != "" && (*it)[0] == '[') {
@@ -2692,6 +2701,7 @@ const section *find_section(const section &sec, const std::string &id)
 void help_browser::show_topic(const std::string &topic_id)
 {
 	const topic *t = find_topic(toplevel_, topic_id);
+
 	if (t != NULL) {
 		show_topic(*t);
 	} else if (topic_id.find(unit_prefix)==0 || topic_id.find(hidden_symbol() + unit_prefix)==0) {
@@ -2704,7 +2714,8 @@ void help_browser::show_topic(const std::string &topic_id)
 
 void help_browser::show_topic(const topic &t, bool save_in_history)
 {
-	log_scope("show_topic");
+	//log_scope("show_topic");
+    lg::scope_logger scope_topic(lg::general, "shop_topic");
 	if (save_in_history) {
 		forward_topics_.clear();
 		if (shown_topic_ != NULL) {
@@ -2714,6 +2725,16 @@ void help_browser::show_topic(const topic &t, bool save_in_history)
 			back_topics_.push_back(shown_topic_);
 		}
 	}
+
+    //if this is a unit help, check if all needed information is available
+    if (t.text.parsed_text().size() == 0){
+        if (t.id.find(unit_prefix) == 0){
+            std::string unit_id = t.id.substr(unit_prefix.length(), t.id.length() - unit_prefix.length());
+            unit_type& type = game_info->unit_types.build_unit_type(unit_id, unit_type::WITHOUT_ANIMATIONS);
+            t.text = new unit_topic_generator(type);
+        }
+    }
+
 	shown_topic_ = &t;
 	text_area_.show_topic(t);
 	menu_.select_topic(t);
@@ -2988,6 +3009,10 @@ void show_help(display &disp, const section &toplevel_sec,
 					 true, &buttons_ptr);
 	f.layout(xloc, yloc, width, height);
 	f.draw();
+
+    // Find all unit_types that have not been constructed yet and fill in the information
+    // needed to create the help topics
+    game_info->unit_types.generate_help_info();
 
 	if (preferences::encountered_units().size() != size_t(last_num_encountered_units) ||
 	    preferences::encountered_terrains().size() != size_t(last_num_encountered_terrains) ||

src/game.cpp
@@ -82,6 +82,7 @@
 #define WRN_CONFIG LOG_STREAM(warn, config)
 #define LOG_CONFIG LOG_STREAM(info, config)
 #define LOG_GENERAL LOG_STREAM(info, general)
+#define DBG_GENERAL LOG_STREAM(debug, general)
 #define ERR_NET LOG_STREAM(err, network)
 #define LOG_NET LOG_STREAM(info, network)
 #define ERR_FS LOG_STREAM(err, filesystem)
@@ -134,8 +135,9 @@ private:
 	game_controller(const game_controller&);
 	void operator=(const game_controller&);
 
-	void read_game_cfg(const preproc_map& defines, config& cfg, bool use_cache);
+	void read_game_cfg(bool use_cache);
 	void refresh_game_cfg(bool reset_translations=false);
+    void set_unit_data();
 
 	void upload_campaign(const std::string& campaign, network::connection sock);
 	void delete_campaign(const std::string& campaign, network::connection sock);
@@ -442,10 +444,7 @@ bool game_controller::init_config()
 	reset_game_cfg();
 	refresh_game_cfg();
 
-	game_config_.clear();
-	read_game_cfg(defines_map_, game_config_, use_caching_);
 	game_config::load_config(game_config_.child("game_config"));
-	old_defines_map_ = defines_map_;
 
 	hotkey::load_hotkeys(game_config_);
 	paths_manager_.set_paths(game_config_);
@@ -646,12 +645,12 @@ bool game_controller::play_multiplayer_mode()
 					if (
 						!faction_choices.empty() &&
 						std::find(faction_choices.begin(),faction_choices.end(),faction_id) == faction_choices.end()
-					) 
+					)
 						continue;
 					if (
 						!faction_excepts.empty() &&
 						std::find(faction_excepts.begin(),faction_excepts.end(),faction_id) != faction_excepts.end()
-					) 
+					)
 						continue;
 					j++;
 					if (rand()%j == 0) {
@@ -1635,7 +1634,7 @@ void game_controller::show_upload_begging()
 }
 
 //this function reads the game configuration, searching for valid cached copies first
-void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, bool use_cache)
+void game_controller::read_game_cfg(bool use_cache)
 {
 	log_scope("read_game_cfg");
 
@@ -1643,7 +1642,7 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 
 	bool is_valid = true;
 	std::stringstream str;
-	for(preproc_map::const_iterator i = defines.begin(); i != defines.end(); ++i) {
+	for(preproc_map::const_iterator i = defines_map_.begin(); i != defines_map_.end(); ++i) {
 		if(i->second.value != "" || i->second.arguments.empty() == false) {
 			is_valid = false;
 			break;
@@ -1687,7 +1686,8 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 				log_scope("read cache");
 				try {
 					scoped_istream stream = istream_file(fname);
-					read_compressed(cfg, *stream);
+					read_compressed(game_config_, *stream);
+					set_unit_data();
 					return;
 				} catch(config::error&) {
 					ERR_CONFIG << "cache is corrupt. Loading from files\n";
@@ -1698,19 +1698,19 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 
 			LOG_CONFIG << "no valid cache found. Writing cache to '" << fname << "'\n";
 
-			preproc_map defines_map(defines);
-			
+			preproc_map defines_map(defines_map_);
+
 			std::string error_log, user_error_log;
 
 			//read the file and then write to the cache
 			scoped_istream stream = preprocess_file("data/", &defines_map, &error_log);
-            
+
 			//reset the parse counter before reading the game files
 			if (loadscreen::global_loadscreen) {
 				loadscreen::global_loadscreen->parser_counter = 0;
 			}
 
-			read(cfg, *stream, &error_log);
+			read(game_config_, *stream, &error_log);
 
 			//load usermade add-ons
 			const std::string user_campaign_dir = get_user_data_dir() + "/data/campaigns/";
@@ -1737,7 +1737,7 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 					read(user_campaign_cfg,*stream,&campaign_error_log);
 
 					if(campaign_error_log.empty()) {
-						cfg.append(user_campaign_cfg);
+						game_config_.append(user_campaign_cfg);
 					} else {
 						user_error_log += campaign_error_log;
 						error_campaigns.push_back(*uc);
@@ -1772,10 +1772,10 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 				gui::show_error_message(disp(),msg.str());
 			}
 
-			cfg.merge_children("units");
+			game_config_.merge_children("units");
 
-			config& hashes = cfg.add_child("multiplayer_hashes");
-			for(config::child_list::const_iterator ch = cfg.get_children("multiplayer").begin(); ch != cfg.get_children("multiplayer").end(); ++ch) {
+			config& hashes = game_config_.add_child("multiplayer_hashes");
+			for(config::child_list::const_iterator ch = game_config_.get_children("multiplayer").begin(); ch != game_config_.get_children("multiplayer").end(); ++ch) {
 				hashes[(**ch)["id"]] = (*ch)->hash();
 			}
 
@@ -1787,7 +1787,7 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 			} else {
 				try {
 					scoped_ostream cache = ostream_file(fname);
-					write_compressed(*cache, cfg);
+					write_compressed(*cache, game_config_);
 					config checksum_cfg;
 					data_tree_checksum().write(checksum_cfg);
 					scoped_ostream checksum = ostream_file(fname_checksum);
@@ -1797,14 +1797,23 @@ void game_controller::read_game_cfg(const preproc_map& defines, config& cfg, boo
 				}
 			}
 
+            set_unit_data();
 			return;
 		}
 	}
 
 	ERR_CONFIG << "caching cannot be done. Reading file\n";
-	preproc_map defines_map(defines);
+	preproc_map defines_map(defines_map_);
 	scoped_istream stream = preprocess_file("data/", &defines_map);
-	read(cfg, *stream);
+	read(game_config_, *stream);
+	set_unit_data();
+}
+
+void game_controller::set_unit_data(){
+    const config* const units = game_config_.child("units");
+    if(units != NULL) {
+        units_data_.set_config(*units);
+    }
 }
 
 void game_controller::refresh_game_cfg(bool reset_translations)
@@ -1816,17 +1825,12 @@ void game_controller::refresh_game_cfg(bool reset_translations)
 			units_data_.clear();
 			if(!reset_translations) {
 				game_config_.clear();
-				read_game_cfg(defines_map_, game_config_, use_caching_);
+				read_game_cfg(use_caching_);
 			} else {
 				game_config_.reset_translation();
 				// we may have translatable strings in [game_config]
 				// e.g. team color names are defined there
 				game_config::load_config(game_config_.child("game_config"));
-			}
-
-			const config* const units = game_config_.child("units");
-			if(units != NULL) {
-				units_data_.set_config(*units);
 			}
 
 			old_defines_map_ = defines_map_;
@@ -2122,10 +2126,10 @@ static int play_game(int argc, char** argv)
 			const std::string output_file(input_file + ".gz");
 
 			try {
-			std::ofstream ofile(output_file.c_str(), std::ios_base::out 
+			std::ofstream ofile(output_file.c_str(), std::ios_base::out
 					| std::ios_base::binary | std::ios_base::binary);
 
-				std::ifstream ifile(input_file.c_str(), 
+				std::ifstream ifile(input_file.c_str(),
 					std::ios_base::in | std::ios_base::binary);
 				boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
 				in.push(boost::iostreams::gzip_compressor());
@@ -2155,10 +2159,10 @@ static int play_game(int argc, char** argv)
 				input_file, 0, input_file.length() - 3);
 
 			try {
-				std::ofstream ofile(output_file.c_str(), std::ios_base::out 
+				std::ofstream ofile(output_file.c_str(), std::ios_base::out
 					| std::ios_base::binary | std::ios_base::binary);
 
-				std::ifstream ifile(input_file.c_str(), 
+				std::ifstream ifile(input_file.c_str(),
 					std::ios_base::in | std::ios_base::binary);
 				boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
 				in.push(boost::iostreams::gzip_decompressor());
@@ -2251,14 +2255,13 @@ static int play_game(int argc, char** argv)
 	loadscreen::global_loadscreen = NULL;
 
 	LOG_CONFIG << "time elapsed: "<<  (SDL_GetTicks() - start_ticks) << " ms\n";
-	
-	//YogiHH:
-	//init_config already processed the configs, so we don't need to do it for the
-	//first loop pass.
+
 	for(int first_time = true;;first_time = false){
+        //init_config already processed the configs, so we don't need to do it for the
+        //first loop pass.
 		if (!first_time){
 			//make sure the game config is always set to how it should be at the title screen
-			game.reset_game_cfg();
+			//game.reset_game_cfg();
 		}
 
 		// reset the TC, since a game can modify it, and it may be used
@@ -2317,7 +2320,6 @@ static int play_game(int argc, char** argv)
 			about::show_about(game.disp());
 			continue;
 		} else if(res == gui::SHOW_HELP) {
-			//game.reset_game_cfg();
 			help::help_manager help_manager(&game.game_config(), &game.units_data(), NULL);
 			help::show_help(game.disp());
 			continue;
@@ -2375,7 +2377,7 @@ int main(int argc, char** argv)
 	} catch(std::bad_alloc&) {
 		std::cerr << "Ran out of memory. Aborted.\n";
 	} catch(twml_exception& e) {
-		std::cerr << "WML exception:\nUser message: " 
+		std::cerr << "WML exception:\nUser message: "
 			<< e.user_message << "\nDev message: " << e.dev_message << '\n';
 	}
 

src/leader_list.cpp
@@ -12,7 +12,7 @@
    See the COPYING file for more details.
 */
 
-//! @file leader_list.cpp 
+//! @file leader_list.cpp
 //! Manage the selection of a leader, and select his/her gender.
 
 #include "global.hpp"
@@ -31,9 +31,9 @@ leader_list_manager::leader_list_manager(const config::child_list& side_list,
 	leaders_(),
 	genders_(),
 	gender_ids_(),
-	side_list_(side_list), 
+	side_list_(side_list),
 	data_(data),
-	leader_combo_(leader_combo), 
+	leader_combo_(leader_combo),
 	gender_combo_(gender_combo)
 {
 }
@@ -126,7 +126,7 @@ void leader_list_manager::update_gender_list(const std::string& leader)
 		return;
 	}
 
-	const game_data::unit_type_map& utypes = data_->unit_types;
+	game_data::unit_type_factory& utypes = data_->unit_types;
 	if (utypes.find(leader) != utypes.end()) {
 		const unit_type* ut;
 		const unit_type* utg;
@@ -165,7 +165,7 @@ void leader_list_manager::update_gender_list(const std::string& leader)
 		if (gender_combo_ != NULL) {
 			gender_combo_->set_items(genders_);
 			gender_index %= genders_.size();
-			gender_combo_->set_selected(gender_index);	
+			gender_combo_->set_selected(gender_index);
 		}
 	} else {
 		gender_ids_.push_back("random");
@@ -173,7 +173,7 @@ void leader_list_manager::update_gender_list(const std::string& leader)
 		if (gender_combo_ != NULL) {
 			gender_combo_->enable(false);
 			gender_combo_->set_items(genders_);
-			gender_combo_->set_selected(0);	
+			gender_combo_->set_selected(0);
 		}
 	}
 }
@@ -183,7 +183,7 @@ void leader_list_manager::populate_leader_combo(int selected_index) {
 	std::vector<std::string> leader_strings;
 	for(itor = leaders_.begin(); itor != leaders_.end(); ++itor) {
 
-		const game_data::unit_type_map& utypes = data_->unit_types;
+		game_data::unit_type_factory& utypes = data_->unit_types;
 
 		//const std::string name = data_->unit_types->find(*itor).language_name();
 		if (utypes.find(*itor) != utypes.end()) {

src/log.cpp
@@ -58,8 +58,7 @@ logger err("error", 0), warn("warning", 1), info("info", 2), debug("debug", 3);
 log_domain general("general"), ai("ai"), config("config"), display("display"),
 	engine("engine"), network("network"), mp_server("server"),
 	filesystem("filesystem"), audio("audio"), notifs("notifs"),
-	replay("replay");
-
+	replay("replay"), help("help");
 log_domain::log_domain(char const *name) : domain_(log_domains.size())
 {
 	logd d = { name, 0 };

src/log.hpp
@@ -50,7 +50,7 @@ std::string get_timestamp(const time_t& t, const std::string& format="%Y%m%d %T 
 
 extern logger err, warn, info, debug;
 extern log_domain general, ai, config, display, engine, network, mp_server,
-	filesystem, audio, notifs, replay;
+	filesystem, audio, notifs, replay, help;
 
 class scope_logger
 {

src/menu_events.cpp
@@ -846,7 +846,7 @@ private:
         // 29.11.07, YogiHH: I can't see why this is needed at all and it
         // breaks some savegame functionality. Probably the original reasons
         // for putting this in are no longer valid.
-        
+
 		// Clobber gold values to make sure the snapshot uses the values
 		// in [side] instead.
 		const config::child_list& players=start.get_children("player");
@@ -1089,7 +1089,7 @@ private:
 					clear_undo_stack(team_num);
 				} else {
 					undo_stack_.push_back(undo_action(new_unit,loc,RECRUIT_POS));
-				} 
+				}
 
 				gui_->recalculate_minimap();
 				gui_->invalidate_game_status();
@@ -1606,8 +1606,8 @@ private:
 			std::stringstream row;
 
 			std::string race;
-			const race_map::const_iterator race_it = gameinfo_.races.find(i->second.race());
-			if (race_it != gameinfo_.races.end()) {
+			const race_map::const_iterator race_it = gameinfo_.unit_types.races().find(i->second.race());
+			if (race_it != gameinfo_.unit_types.races().end()) {
 				race = race_it->second.plural_name();
 			}
 			row << race << COLUMN_SEPARATOR;
@@ -2128,7 +2128,7 @@ private:
 			const std::string side_s(data.begin(),j);
 			const std::string action(j,data.end());
 			// default to the current side if empty
-			const unsigned int side = side_s.empty() ? 
+			const unsigned int side = side_s.empty() ?
 				team_num : lexical_cast_default<unsigned int>(side_s);
 
 			if (side < 1 || side > teams_.size()) {
@@ -2267,7 +2267,7 @@ private:
 			// to an enum, then used to index an
 			// array of strings.
 			// But someday the code ought to be
-			// changed to allow general string 
+			// changed to allow general string
 			// alignments for UMC.
 			if (name == "alignment" && (value != "lawful" && value != "neutral" && value != "chaotic")) {
 				ERR_NG << "Invalid alignment: '" << value

src/multiplayer_wait.cpp
@@ -95,7 +95,7 @@ void wait::leader_preview_pane::draw_contents()
 		std::string leader = leaders_.get_leader();
 		std::string gender = leaders_.get_gender();
 
-		const game_data::unit_type_map& utypes = data_->unit_types;
+		game_data::unit_type_factory& utypes = data_->unit_types;
 		std::string leader_name;
 		std::string image;
 
@@ -231,7 +231,7 @@ void wait::join_game(bool observe)
 
 	// Add the map name to the title.
 	append_to_title(": " + level_["name"]);
-	
+
 	if (!observe) {
 		const config::child_list& sides_list = level_.get_children("side");
 
@@ -329,7 +329,7 @@ void wait::start_game()
 
 
 
-	//! @todo Instead of using level_to_gamestate reinit the state_, 
+	//! @todo Instead of using level_to_gamestate reinit the state_,
 	//! this needs more testing -- Mordante
 	//! It seems level_to_gamestate is needed for the start of game
 	//! download, but downloads of later scenarios miss certain info
@@ -339,11 +339,11 @@ void wait::start_game()
 	if(level_.child("player") == 0) {
 		level_to_gamestate(level_, state_, level_["savegame"] == "yes");
 	} else {
-	
+
 		state_ = game_state(game_data_, level_);
 
-		// When we observe and don't have the addon installed we still need 
-		// the old way, no clue why however. Code is a copy paste of 
+		// When we observe and don't have the addon installed we still need
+		// the old way, no clue why however. Code is a copy paste of
 		// playcampaign.cpp:576 which shows an 'Unknown scenario: '$scenario|'
 		// error. This seems to work and have no side effects....
 		if(!state_.scenario.empty() && state_.scenario != "null") {
@@ -466,7 +466,7 @@ void wait::generate_menu()
 
 		std::string leader_name;
 		std::string leader_image;
-		const game_data::unit_type_map& utypes = game_data_.unit_types;
+		game_data::unit_type_factory& utypes = game_data_.unit_types;
 		const unit_type* ut;
 		const unit_type* utg;
 

commit 7a1aa7abb96d463a485d697020d37458ddc19223

Fix "unit descrition" action for unit with hide_help=yes Was broken by r25212 (unit lazy loading avoiding an old topic lazy loading)
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@27951 75b3775d-3801-0410-9e12-e2e43121521d

src/help.cpp
@@ -1193,6 +1193,9 @@ class unit_topic_generator: public topic_generator
 public:
 	unit_topic_generator(const unit_type &t): type_(t) {}
 	virtual std::string operator()() const {
+		// this will force the lazy loading to build this unit
+		unit_type_data::types().find(type_.id(), unit_type::WITHOUT_ANIMATIONS);
+		
 		std::stringstream ss;
 		std::string clear_stringstream;
 		const std::string detailed_description = type_.unit_description();
@@ -1574,7 +1577,7 @@ std::vector<topic> generate_unit_topics(const bool sort_generated, const std::st
 		const std::string type_name = type.type_name();
 		const std::string ref_id = hidden_symbol(type.hide_help()) + unit_prefix +  type.id();
 		topic unit_topic(type_name, ref_id, "");
-		//unit_topic.text = new unit_topic_generator(type);
+		unit_topic.text = new unit_topic_generator(type);
 		topics.push_back(unit_topic);
 
 		if (!type.hide_help()) {
@@ -2709,15 +2712,6 @@ void help_browser::show_topic(const topic &t, bool save_in_history)
 			back_topics_.push_back(shown_topic_);
 		}
 	}
-
-    //if this is a unit help, check if all needed information is available
-    if (t.text.parsed_text().size() == 0){
-        if (t.id.find(unit_prefix) == 0){
-            std::string unit_id = t.id.substr(unit_prefix.length(), t.id.length() - unit_prefix.length());
-            const unit_type& type = unit_type_data::types().find(unit_id, unit_type::WITHOUT_ANIMATIONS)->second;
-            t.text = new unit_topic_generator(type);
-        }
-    }
 
 	shown_topic_ = &t;
 	text_area_.show_topic(t);