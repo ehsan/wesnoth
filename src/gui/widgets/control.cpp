ehsan/wesnoth

commit 87fd09f792a5b054b5e5604919ef05ddc5ec2e10

Remove the wrap since it's triggered automatically when setting the width of the Pango layout.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@28424 75b3775d-3801-0410-9e12-e2e43121521d

src/gui/widgets/control.cpp
@@ -230,7 +230,7 @@ void tcontrol::restore_background(surface& dst)
 	gui2::restore_background(restorer_, dst, get_rect());
 }
 
-tpoint tcontrol::get_best_text_size(const tpoint& minimum_size, const bool word_wrap) const
+tpoint tcontrol::get_best_text_size(const tpoint& minimum_size) const
 {
 	assert(!label_.empty());
 
@@ -240,7 +240,6 @@ tpoint tcontrol::get_best_text_size(const tpoint& minimum_size, const bool word_
 	font::ttext text;
 	text.set_text(label_, false);
 	text.set_font_size(config_->text_font_size);
-	text.set_word_wrap(word_wrap);
 
 	// Try with the minimum wanted size.
 	text.set_maximum_width(size.x);

src/gui/widgets/control.hpp
@@ -304,12 +304,10 @@ private:
 	 * Gets the best size for a text.
 	 *
 	 * @param minimum_size        The minimum size of the text.
-	 * @param word_wrap           Are we allowed to wrap the lines.
 	 *
 	 * @returns                   The best size.
 	 */
-	tpoint get_best_text_size(const tpoint& minimum_size, 
-		const bool word_wrap) const;
+	tpoint get_best_text_size(const tpoint& minimum_size) const;
 };
 
 } // namespace gui2

src/text.cpp
@@ -37,7 +37,6 @@ ttext::ttext() :
 	foreground_colour_(0xFFFFFFFF), // solid white
 	maximum_width_(-1),
 	maximum_height_(-1),
-	word_wrap_(false),
 	calculation_dirty_(true),
 	surface_dirty_(true),
 	surface_buffer_(NULL)
@@ -154,19 +153,6 @@ ttext& ttext::set_maximum_height(const int height)
 		pango_layout_set_height(layout_, height);
 #endif
 		maximum_height_ = height;
-		calculation_dirty_ = true;
-		surface_dirty_ = true;
-	}
-
-	return *this;
-}
-
-ttext& ttext::set_word_wrap(const bool wrap)
-{
-	if(wrap != word_wrap_) {
-		pango_layout_set_wrap(layout_, wrap 
-			? PANGO_WRAP_WORD_CHAR : static_cast<PangoWrapMode>(-1));
-		word_wrap_ = wrap;
 		calculation_dirty_ = true;
 		surface_dirty_ = true;
 	}

src/text.hpp
@@ -76,8 +76,6 @@ public:
 
 	ttext& set_maximum_height(const int height);
 
-	ttext& set_word_wrap(const bool wrap);
-
 private:
 
 	/***** ***** ***** *****  Pango variables ***** ***** ***** *****/
@@ -107,9 +105,6 @@ private:
 
 	/** The maximum height of the text, -1 means no maximum . */
 	int maximum_height_;
-
-	/** Do we enable word wrap for the text, if enable the word char mode is set. */
-	bool word_wrap_;
 
 	/** 
 	 * The text has two dirty states:

commit 53f430dd80262b75c6dde03be00522dc0ec8094a

Fix a compilation error, caused by r28424.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@28425 75b3775d-3801-0410-9e12-e2e43121521d

src/gui/widgets/control.cpp
@@ -109,7 +109,7 @@ tpoint tcontrol::get_minimum_size() const
 		return min_size;
 	}
 
-	return get_best_text_size(min_size, multiline_label_);
+	return get_best_text_size(min_size);
 }
 
 tpoint tcontrol::get_best_size() const
@@ -122,7 +122,7 @@ tpoint tcontrol::get_best_size() const
 		return default_size;
 	}
 
-	return get_best_text_size(default_size, multiline_label_);
+	return get_best_text_size(default_size);
 }
 
 tpoint tcontrol::get_maximum_size() const