ehsan/wesnoth

commit 260e1f701a8b9edc82da735ee2aca1e613f9c61d

Got rid of pointers to config objects. Removed code duplication along the way.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@34487 75b3775d-3801-0410-9e12-e2e43121521d

src/multiplayer.cpp
@@ -150,11 +150,10 @@ static server_type open_connection(game_display& disp, const std::string& origin
 		}
 
 		// Check for "redirect" messages
-		if(data.child("redirect")) {
-			config* redirect = data.child("redirect");
-
-			host = (*redirect)["host"];
-			port = lexical_cast_default<unsigned int>((*redirect)["port"], 15000);
+		if (const config &redirect = data.child("redirect"))
+		{
+			host = redirect["host"];
+			port = lexical_cast_default<unsigned int>(redirect["port"], 15000);
 
 			if(shown_hosts.find(hostpair(host,port)) != shown_hosts.end()) {
 				throw network::error(_("Server-side redirect loop"));
@@ -207,10 +206,10 @@ static server_type open_connection(game_display& disp, const std::string& origin
 					throw network::error(_("Connection timed out"));
 				}
 
-				config* error = data.child("error");
+				config *error = &data.child("error");
 
 				// ... and get us out of here if the server did not complain
-				if(!error) break;
+				if (!*error) break;
 
 				do {
 					std::string password = preferences::password();
@@ -262,10 +261,10 @@ static server_type open_connection(game_display& disp, const std::string& origin
 							throw network::error(_("Connection timed out"));
 						}
 
-						error = data.child("error");
+						error = &data.child("error");
 
 						// ... and get us out of here if the server is happy now
-						if(!error) break;
+						if (!*error) break;
 
 
 					}

src/addon_management.cpp
@@ -467,16 +467,16 @@ namespace {
 		if(!sock) {
 			gui::show_error_message(disp, _("Connection timed out"));
 			return;
-		} else if(data.child("error")) {
+		} else if (const config &c = data.child("error")) {
 			std::string error_message = _("The server responded with an error: \"$error|\"");
 			utils::string_map symbols;
-			symbols["error"] = (*data.child("error"))["message"].str();
+			symbols["error"] = c["message"].str();
 			error_message = utils::interpolate_variables_into_string(error_message, &symbols);
 			gui::show_error_message(disp, error_message);
 			return;
-		} else if(data.child("message")) {
+		} else if (const config &c = data.child("message")) {
 			/* GCC-3.3 needs a temp var otherwise compilation fails */
-			gui::dialog dlg(disp,_("Terms"),(*data.child("message"))["message"],gui::OK_CANCEL);
+			gui::dialog dlg(disp,_("Terms"), c["message"], gui::OK_CANCEL);
 			const int res = dlg.show();
 			if(res != 0) {
 				return;
@@ -513,12 +513,12 @@ namespace {
 		sock = dialogs::network_send_dialog(disp,_("Sending add-on"),data,sock);
 		if(!sock) {
 			return;
-		} else if(data.child("error")) {
+		} else if (const config &c = data.child("error")) {
 			gui::show_error_message(disp, _("The server responded with an error: \"") +
-									(*data.child("error"))["message"].str() + '"');
-		} else if(data.child("message")) {
+			                        c["message"].str() + '"');
+		} else if (const config &c = data.child("message")) {
 			/* GCC-3.3 needs a temp var otherwise compilation fails */
-			gui::message_dialog dlg(disp,_("Response"),(*data.child("message"))["message"]);
+			gui::message_dialog dlg(disp, _("Response"), c["message"]);
 			dlg.show();
 		}
 	}
@@ -540,12 +540,12 @@ namespace {
 		sock = network::receive_data(data,sock,5000);
 		if(!sock) {
 			gui::show_error_message(disp, _("Connection timed out"));
-		} else if(data.child("error")) {
+		} else if (const config &c = data.child("error")) {
 			gui::show_error_message(disp, _("The server responded with an error: \"") +
-									(*data.child("error"))["message"].str() + '"');
-		} else if(data.child("message")) {
+			                        c["message"].str() + '"');
+		} else if (const config &c = data.child("message")) {
 			/* GCC-3.3 needs a temp var otherwise compilation fails */
-			gui::message_dialog dlg(disp,_("Response"),(*data.child("message"))["message"]);
+			gui::message_dialog dlg(disp, _("Response"), c["message"]);
 			dlg.show();
 		}
 	}
@@ -559,11 +559,11 @@ namespace {
 	                   bool show_result = true)
 	{
 		// Get all dependencies of the addon/campaign selected for download.
-		const config * const selected_campaign = addons_tree.find_child("campaign", "name", addon_id);
-		assert(selected_campaign != NULL);
+		const config &selected_campaign = addons_tree.find_child("campaign", "name", addon_id);
+		assert(selected_campaign);
 		// Get all dependencies which are not already installed.
 		// TODO: Somehow determine if the version is outdated.
-		std::vector<std::string> dependencies = utils::split((*selected_campaign)["dependencies"]);
+		std::vector<std::string> dependencies = utils::split(selected_campaign["dependencies"]);
 		if (!addon_dependencies_met(disp,dependencies)) return false;
 		// Proceed to download and install
 		config request;
@@ -582,9 +582,8 @@ namespace {
 			return false;
 		}
 
-		config const * const dlerror = cfg.child("error");
-		if(dlerror != NULL) {
-			gui::show_error_message(disp, (*dlerror)["message"]);
+		if (config const &dlerror = cfg.child("error")) {
+			gui::show_error_message(disp, dlerror["message"]);
 			return false;
 		}
 
@@ -594,8 +593,8 @@ namespace {
 		}
 
 		// add revision info to the addon archive
-		config* maindir = cfg.find_child("dir", "name", addon_id);
-		if(maindir == NULL) {
+		config *maindir = &cfg.find_child("dir", "name", addon_id);
+		if (!*maindir) {
 			LOG_CFG << "downloaded addon '" << addon_id << "' is missing its own directory, creating...\n";
 			maindir = &cfg.add_child("dir");
 			(*maindir)["name"] = addon_id;
@@ -821,12 +820,12 @@ namespace {
 		bool result = true;
 		std::vector<std::string> failed_titles;
 
-		assert(cfg.child("campaigns") != NULL);
+		assert(cfg.child("campaigns"));
 
 		if(upd_all) {
 			for(size_t i = 0; i < addons.size() && i < remote_matches_cfgs.size(); ++i)
 			{
-				if(!install_addon(disp, * cfg.child("campaigns"), addons[i], titles[i],
+				if (!install_addon(disp, cfg.child("campaigns"), addons[i], titles[i],
 				                   types[i], uploads[i], newversions[i], net_manager, sock,
 				                   do_refresh, false)) {
 					result=false;
@@ -835,7 +834,7 @@ namespace {
 			}
 		} else {
 			const size_t i = static_cast<size_t>(index);
-			if(!install_addon(disp, * cfg.child("campaigns"), addons[i], titles[i],
+			if (!install_addon(disp, cfg.child("campaigns"), addons[i], titles[i],
 				               types[i], uploads[i], newversions[i], net_manager, sock,
 				               do_refresh, false)) {
 				result=false;
@@ -903,19 +902,18 @@ namespace {
 				return;
 			}
 
-			config const * const error = cfg.child("error");
-			if(error != NULL) {
-				gui::show_error_message(disp, (*error)["message"]);
+			if (config const &error = cfg.child("error")) {
+				gui::show_error_message(disp, error["message"]);
 				return;
 			}
 
-			config const * const addons_tree = cfg.child("campaigns");
-			if(addons_tree == NULL) {
+			config const &addons_tree = cfg.child("campaigns");
+			if (!addons_tree) {
 				gui::show_error_message(disp, _("An error occurred while communicating with the server."));
 				return;
 			}
 
-			const config::child_list& addon_cfgs = addons_tree->get_children("campaign");
+			const config::child_list &addon_cfgs = addons_tree.get_children("campaign");
 			if(update_mode) {
 				addons_update_dlg(disp, cfg, addon_cfgs, net_manager, sock, do_refresh);
 				return;
@@ -1013,7 +1011,7 @@ namespace {
 
 			int index = -1;
 			if(gui2::new_widgets) {
-				gui2::taddon_list dlg(*addons_tree);
+				gui2::taddon_list dlg(addons_tree);
 				dlg.show(disp.video());
 			} else {
 
@@ -1062,7 +1060,7 @@ namespace {
 			}
 
 			// Handle download
-			install_addon(disp, * addons_tree, addons[index], titles[index], types[index],
+			install_addon(disp, addons_tree, addons[index], titles[index], types[index],
 			              uploads[index], versions[index], net_manager, sock, do_refresh);
 
 			// Show the dialog again, and position it on the same item installed
@@ -1236,11 +1234,11 @@ void refresh_addon_version_info_cache()
 			scoped_istream stream = istream_file(info_file);
 			read(cfg, *stream);
 
-			config const* const info_cfg = cfg.child("info");
-			if(info_cfg == NULL) {
+			config const &info_cfg = cfg.child("info");
+			if (!info_cfg) {
 				continue;
 			}
-			std::string const& version = (*info_cfg)["version"];
+			std::string const& version = info_cfg["version"];
 			LOG_CFG << "caching add-on version info: " << addon << " [" << version << "]\n";
 			version_info_cache.insert(std::make_pair(addon, version_info(version)));
 		}

src/ai.cpp
@@ -883,7 +883,7 @@ void ai::find_threats()
 	foreach (const config &p, parms.child_range("protect_unit"))
 	{
 		for(unit_map::const_iterator u = units_.begin(); u != units_.end(); ++u) {
-			if (game_events::unit_matches_filter(u, &p)) {
+			if (game_events::unit_matches_filter(u, vconfig(p))) {
 				items.push_back(protected_item(
 							lexical_cast_default<double>(p["value"], 1.0),
 							lexical_cast_default<int>(p["radius"], 20),
@@ -1980,14 +1980,14 @@ bool ai::do_recruitment()
 
 void ai::move_leader_to_goals( const move_map& enemy_dstsrc)
 {
-	const config* const goal = current_team().ai_parameters().child("leader_goal");
+	const config &goal = current_team().ai_parameters().child("leader_goal");
 
-	if(goal == NULL) {
+	if (!goal) {
 		LOG_AI << "No goal found\n";
 		return;
 	}
 
-	const map_location dst(*goal, &get_info().game_state_);
+	const map_location dst(goal, &get_info().game_state_);
 	if (!dst.valid()) {
 		ERR_AI << "Invalid goal\n";
 		return;

src/ai_dfool.cpp
@@ -135,7 +135,7 @@ namespace dfool {
 	      config ff=**sf;
 	      LOG_STREAM(info, ai)<<"ff:"<<(**com)["type"]<<" "<<ff["type"]<<" "<<ff["x"]<<","<<ff["y"]<<std::endl;
 	      LOG_STREAM(info, ai)<<"ff?"<<u->second.type_id()<<" "<<u->first.x<<","<<u->first.y<<std::endl;
-	      if(! u->second.matches_filter(&ff,u->first)) {
+	      if (!u->second.matches_filter(vconfig(ff), u->first)) {
 		found=false;
 		break;
 	      }
@@ -193,7 +193,7 @@ namespace dfool {
       //      LOG_STREAM(info, ai)<<"j:"<<j->second.underlying_id()<<":"<<j->first.x<<","<<j->first.y<<std::endl;
       assert(j->second.underlying_id() >0);
 	//	LOG_STREAM(info, ai)<<"filter3:"<<std::endl;
-	if(j->second.matches_filter(&filter,j->first)) {
+	if (j->second.matches_filter(vconfig(filter), j->first)) {
 	  //	  LOG_STREAM(info, ai)<<"filter4:"<<std::endl;
 	  filtered_units_.push_back(*i);
 	}

src/ai_move.cpp
@@ -191,7 +191,7 @@ std::vector<ai::target> ai::find_targets(unit_map::const_iterator leader, const 
 		//explicit targets for this team
 		for(std::vector<team::target>::iterator j = team_targets.begin();
 		    j != team_targets.end(); ++j) {
-			if(u->second.matches_filter(&(j->criteria),u->first)) {
+			if (u->second.matches_filter(vconfig(j->criteria), u->first)) {
 				LOG_AI << "found explicit target... " << u->first << " with value: " << j->value << "\n";
 				targets.push_back(target(u->first,j->value,target::EXPLICIT));
 			}

src/cavegen.cpp
@@ -28,7 +28,7 @@
 
 #define LOG_NG LOG_STREAM(info, engine)
 
-cave_map_generator::cave_map_generator(const config* cfg) :
+cave_map_generator::cave_map_generator(const config &cfg) :
 	wall_(t_translation::CAVE_WALL),
 	clear_(t_translation::CAVE),
 	village_(t_translation::UNDERGROUND_VILLAGE),
@@ -40,33 +40,27 @@ cave_map_generator::cave_map_generator(const config* cfg) :
 	chambers_(),
 	passages_(),
 	res_(),
-	cfg_(cfg),
+	cfg_(cfg ? cfg : config()),
 	width_(50),
 	height_(50),
 	village_density_(0),
 	flipx_(false),
 	flipy_(false)
 {
-	if(cfg_ == NULL) {
-		static const config default_cfg;
-		cfg_ = &default_cfg;
-	}
-
-	width_ = atoi((*cfg_)["map_width"].c_str());
-	height_ = atoi((*cfg_)["map_height"].c_str());
+	width_ = atoi(cfg_["map_width"].c_str());
+	height_ = atoi(cfg_["map_height"].c_str());
 	width_ += 2 * gamemap::default_border;
 	height_ += 2 * gamemap::default_border;
 
-	village_density_ = atoi((*cfg_)["village_density"].c_str());
+	village_density_ = atoi(cfg_["village_density"].c_str());
 
 	const int r = rand()%100;
-	const int chance = atoi((*cfg_)["flipx_chance"].c_str());
+	const int chance = atoi(cfg_["flipx_chance"].c_str());
 
 	flipx_ = r < chance;
 
 	LOG_NG << "flipx: " << r << " < " << chance << " = " << (flipx_ ? "true" : "false") << "\n";
-	flipy_ = (rand()%100) < atoi((*cfg_)["flipy_chance"].c_str());
-
+	flipy_ = (rand()%100) < atoi(cfg_["flipy_chance"].c_str());
 }
 
 std::string cave_map_generator::config_name() const
@@ -105,9 +99,8 @@ config cave_map_generator::create_scenario(const std::vector<std::string>& /*arg
 	passages_.clear();
 
 	res_.clear();
-	const config* const settings = cfg_->child("settings");
-	if(settings != NULL) {
-		res_ = *settings;
+	if (const config &settings = cfg_.child("settings")) {
+		res_ = settings;
 	}
 
 	LOG_NG << "creating scenario....\n";
@@ -152,7 +145,7 @@ void cave_map_generator::build_chamber(map_location loc, std::set<map_location>&
 
 void cave_map_generator::generate_chambers()
 {
-	foreach (const config &ch, cfg_->child_range("chamber"))
+	foreach (const config &ch, cfg_.child_range("chamber"))
 	{
 		// If there is only a chance of the chamber appearing, deal with that here.
 		const std::string &chance = ch["chance"];
@@ -191,7 +184,8 @@ void cave_map_generator::generate_chambers()
 		new_chamber.center = map_location(x,y);
 		build_chamber(new_chamber.center,new_chamber.locs,chamber_size,jagged_edges);
 
-		new_chamber.items = ch.child("items");
+		const config &items = ch.child("items");
+		new_chamber.items = items ? &items : NULL;
 
 		const std::string &id = ch["id"];
 		if (!id.empty()) {
@@ -228,11 +222,11 @@ void cave_map_generator::place_chamber(const chamber& c)
 	foreach (const config::any_child &it, c.items->all_children_range())
 	{
 		config cfg = it.cfg;
-		config* const filter = cfg.child("filter");
-		config* const object = cfg.child("object");
+		config &filter = cfg.child("filter");
 		config* object_filter = NULL;
-		if(object != NULL) {
-			object_filter = object->child("filter");
+		if (config &object = cfg.child("object")) {
+			if (config &of = object.child("filter"))
+				object_filter = &of;
 		}
 
 		if(!utils::string_bool(cfg["same_location_as_previous"])) {
@@ -246,8 +240,8 @@ void cave_map_generator::place_chamber(const chamber& c)
 		char xbuf[50];
 		snprintf(xbuf,sizeof(xbuf),"%d",loc->x+1);
 		cfg["x"] = xbuf;
-		if(filter != NULL) {
-			(*filter)["x"] = xbuf;
+		if (filter) {
+			filter["x"] = xbuf;
 		}
 
 		if(object_filter != NULL) {
@@ -257,8 +251,8 @@ void cave_map_generator::place_chamber(const chamber& c)
 		char ybuf[50];
 		snprintf(ybuf,sizeof(ybuf),"%d",loc->y+1);
 		cfg["y"] = ybuf;
-		if(filter != NULL) {
-			(*filter)["y"] = ybuf;
+		if (filter) {
+			filter["y"] = ybuf;
 		}
 
 		if(object_filter != NULL) {

src/cavegen.hpp
@@ -25,7 +25,7 @@
 class cave_map_generator : public map_generator
 {
 public:
-	cave_map_generator(const config* game_config);
+	cave_map_generator(const config &game_config);
 
 	bool allow_user_config() const { return true; }
 	// This is a pure virtual function in the base class, so must be here
@@ -88,7 +88,7 @@ private:
 	std::vector<passage> passages_;
 
 	config res_;
-	const config* cfg_;
+	config cfg_;
 	size_t width_, height_, village_density_;
 
 	// The scenario may have a chance to flip all x values or y values

commit 607ac7547a40726b0c577308abf2d255fb78e609

fix bug #13478 introduced in r34487
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@35457 75b3775d-3801-0410-9e12-e2e43121521d

src/multiplayer.cpp
@@ -333,7 +333,7 @@ static server_type open_connection(game_display& disp, const std::string& origin
 				// Somewhat hacky...
 				// If we broke out of the do-while loop above error
 				// is still going to be NULL
-				if(!error) break;
+				if(!*error) break;
 			} // end login loop
 		}
 	} while(!(data.child("join_lobby") || data.child("join_game")));