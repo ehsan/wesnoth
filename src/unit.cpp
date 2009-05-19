ehsan/wesnoth

commit 9f44213d6823fbb7546b91642c60616f70ead9b5

Added a content-addressable-table of unit states which are boolean and are used by the game. This allows O(1) lookup of these unit states, and give ~ +10% to AI speed (more with --nogui), due to get_state("slowed") replaced with get_state(STATE_SLOWED) in unit::movement_cost calculation which is heavily used by the pathfinding. This does the improvement described in Bug #13520
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@35630 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -124,6 +124,7 @@ unit::unit(const unit& o):
            max_attacks_(o.max_attacks_),
 
            states_(o.states_),
+           known_boolean_states_(o.known_boolean_states_),
            variables_(o.variables_),
            emit_zoc_(o.emit_zoc_),
            state_(o.state_),
@@ -206,6 +207,7 @@ unit::unit(unit_map* unitmap, const gamemap* map, const gamestatus* game_status,
 	attacks_left_(0),
 	max_attacks_(0),
 	states_(),
+	known_boolean_states_(known_boolean_state_names_.size(),false),
 	variables_(),
 	emit_zoc_(0),
 	state_(STATE_STANDING),
@@ -285,6 +287,7 @@ unit::unit(const config& cfg,bool use_traits) :
 	attacks_left_(0),
 	max_attacks_(0),
 	states_(),
+	known_boolean_states_(known_boolean_state_names_.size(),false),
 	variables_(),
 	emit_zoc_(0),
 	state_(STATE_STANDING),
@@ -392,6 +395,7 @@ unit::unit(unit_map* unitmap, const gamemap* map, const gamestatus* game_status,
 	attacks_left_(0),
 	max_attacks_(0),
 	states_(),
+	known_boolean_states_(known_boolean_state_names_.size(),false),
 	variables_(),
 	emit_zoc_(0),
 	state_(STATE_STANDING),
@@ -491,6 +495,7 @@ unit::unit(const unit_type* t, int side, bool use_traits, bool dummy_unit,
 	attacks_left_(0),
 	max_attacks_(0),
 	states_(),
+	known_boolean_states_(known_boolean_state_names_.size(),false),
 	variables_(),
 	emit_zoc_(0),
 	state_(STATE_STANDING),
@@ -771,9 +776,9 @@ void unit::advance_to(const unit_type* t, bool use_traits, game_state* state)
 
 	game_events::add_events(cfg_.child_range("event"), type_);
 
-	set_state("poisoned","");
-	set_state("slowed","");
-	set_state("petrified","");
+	set_state(STATE_POISONED,false);
+	set_state(STATE_SLOWED,false);
+	set_state(STATE_PETRIFIED,false);
 	end_turn_ = false;
 	refreshing_  = false;
 	hidden_ = false;
@@ -890,7 +895,7 @@ void unit::new_turn()
 	end_turn_ = false;
 	movement_ = total_movement();
 	attacks_left_ = max_attacks_;
-	set_state("hidden","yes");
+	set_state(STATE_HIDDEN,true);
 
 	if (hold_position_) {
 		end_turn_ = true;
@@ -898,11 +903,11 @@ void unit::new_turn()
 }
 void unit::end_turn()
 {
-	set_state("slowed","");
-	if((movement_ != total_movement()) && !utils::string_bool(get_state("not_moved")) && (!is_healthy_ || attacks_left_ < max_attacks_)) {
+	set_state(STATE_SLOWED,false);
+	if((movement_ != total_movement()) && !(get_state(STATE_NOT_MOVED)) && (!is_healthy_ || attacks_left_ < max_attacks_)) {
 		resting_ = false;
 	}
-	set_state("not_moved","");
+	set_state(STATE_NOT_MOVED,false);
 	// Clear interrupted move
 	set_interrupted_move(map_location());
 }
@@ -916,9 +921,9 @@ void unit::new_scenario()
 	remove_temporary_modifications();
 
 	heal_all();
-	set_state("slowed","");
-	set_state("poisoned","");
-	set_state("petrified","");
+	set_state(STATE_SLOWED,false);
+	set_state(STATE_POISONED,false);
+	set_state(STATE_PETRIFIED,false);
 }
 void unit::remove_temporary_modifications()
 {
@@ -957,27 +962,80 @@ void unit::heal(int amount)
 	}
 }
 
-const std::map<std::string,std::string>& unit::get_states() const
+const std::map<std::string,std::string> unit::get_states() const
 {
-	return states_;
+	std::map<std::string,std::string> all_states = states_;
+	for (std::map<std::string,size_t>::const_iterator i = known_boolean_state_names_.begin(); i!=known_boolean_state_names_.end(); ++i){
+		if (get_state(i->second)){	
+			all_states.insert(make_pair(i->first, "yes" ));
+		}
+	
+	}
+	return all_states;
 }
 
 std::string unit::get_state(const std::string& state) const
 {
+	size_t known_boolean_state_id = get_known_boolean_state_id(state);
+	if (known_boolean_state_id!=STATE_UNKNOWN){
+		return get_state(known_boolean_state_id) ? "yes" : "";
+	}
 	std::map<std::string,std::string>::const_iterator i = states_.find(state);
 	if(i != states_.end()) {
 		return i->second;
 	}
 	return "";
 }
+
+void unit::set_state(size_t state, bool value)
+{
+	known_boolean_states_[state] = value;
+}
+
+bool unit::get_state(size_t state) const
+{
+	return known_boolean_states_[state];
+}
+
+size_t unit::get_known_boolean_state_id(const std::string &state) {
+	std::map<std::string,size_t>::const_iterator i = known_boolean_state_names_.find(state);
+	if (i !=known_boolean_state_names_.end()){
+		return i->second;
+	}	
+	return STATE_UNKNOWN;	
+}
+
+std::map<std::string,size_t> unit::known_boolean_state_names_ = get_known_boolean_state_names();
+
+std::map<std::string,size_t> unit::get_known_boolean_state_names(){
+	std::map<std::string,size_t> *known_boolean_state_names_map = new std::map<std::string,size_t>();
+	known_boolean_state_names_map->insert(std::make_pair("slowed",STATE_SLOWED));
+	known_boolean_state_names_map->insert(std::make_pair("poisoned",STATE_POISONED));
+	known_boolean_state_names_map->insert(std::make_pair("petrified",STATE_PETRIFIED));
+	known_boolean_state_names_map->insert(std::make_pair("hidden",STATE_HIDDEN));
+	known_boolean_state_names_map->insert(std::make_pair("not_moved",STATE_NOT_MOVED));
+	//not sure if "guardian" is a yes/no state.
+	//known_boolean_state_names_map->insert(std::make_pair("guardian",STATE_GUARDIAN));
+	return *known_boolean_state_names_map;
+}
+
 void unit::set_state(const std::string& state, const std::string& value)
 {
+	size_t known_boolean_state_id = get_known_boolean_state_id(state);
 	if(value == "") {
+		if (known_boolean_state_id!=STATE_UNKNOWN){
+			set_state(known_boolean_state_id,false);
+			return;
+		}
 		std::map<std::string,std::string>::iterator i = states_.find(state);
 		if(i != states_.end()) {
 			states_.erase(i);
 		}
 	} else {
+		if (known_boolean_state_id!=STATE_UNKNOWN){
+			set_state(known_boolean_state_id,true);
+			return;
+		}
 		states_[state] = value;
 	}
 }
@@ -1506,7 +1564,7 @@ void unit::read(const config& cfg, bool use_traits, game_state* state)
 	if (const config &status_flags = cfg.child("status"))
 	{
 		foreach (const config::attribute &st, status_flags.attribute_range()) {
-			states_[st.first] = st.second;
+			set_state(st.first,st.second);
 		}
 		cfg_.remove_child("status",0);
 	}
@@ -1642,7 +1700,8 @@ void unit::write(config& cfg) const
 	cfg["flying"] = flying_ ? "yes" : "no";
 
 	config status_flags;
-	for(std::map<std::string,std::string>::const_iterator st = states_.begin(); st != states_.end(); ++st) {
+	std::map<std::string,std::string> all_states = get_states();
+	for(std::map<std::string,std::string>::const_iterator st = all_states.begin(); st != all_states.end(); ++st) {
 		status_flags[st->first] = st->second;
 	}
 
@@ -1835,7 +1894,7 @@ void unit::redraw_unit(game_display& disp, const map_location& loc)
 	}
 	params.y -= height_adjust;
 	params.halo_y -= height_adjust;
-	if (utils::string_bool(get_state("poisoned")) ){
+	if (get_state(STATE_POISONED)){
 		params.blend_with = disp.rgb(0,255,0);
 		params.blend_ratio = 0.25;
 	}
@@ -1843,7 +1902,7 @@ void unit::redraw_unit(game_display& disp, const map_location& loc)
 	params.image= absolute_image();
 
 
-	if(utils::string_bool(get_state("petrified"))) params.image_mod +="~GS()";
+	if(get_state(STATE_PETRIFIED)) params.image_mod +="~GS()";
 
 	const frame_parameters adjusted_params = anim_->get_current_params(params,true);
 
@@ -2032,7 +2091,7 @@ bool unit::invalidate(const map_location &loc)
 		}
 		params.y -= height_adjust;
 		params.halo_y -= height_adjust;
-		if (utils::string_bool(get_state("poisoned")) ){
+		if (get_state(STATE_POISONED) ){
 			params.blend_with = disp->rgb(0,255,0);
 			params.blend_ratio = 0.25;
 		}
@@ -2134,7 +2193,7 @@ int unit::movement_cost_internal(const t_translation::t_terrain terrain, const i
 int unit::movement_cost(const t_translation::t_terrain terrain) const
 {
 	const int res = movement_cost_internal(terrain, 0);
-	if(utils::string_bool(get_state("slowed"))) {
+	if(get_state(STATE_SLOWED)) {
 		return res*2;
 	}
 	return res;
@@ -2903,7 +2962,7 @@ bool unit::invisible(const map_location& loc,
 
 	// Test hidden status
 	static const std::string hides("hides");
-	bool is_inv = (utils::string_bool(get_state("hidden")) && get_ability_bool(hides,loc));
+	bool is_inv = get_state(STATE_HIDDEN && get_ability_bool(hides,loc));
 	if(is_inv){
 		for(unit_map::const_iterator u = units.begin(); u != units.end(); ++u) {
 			if(teams[side_-1].is_enemy(u->second.side()) && tiles_adjacent(loc,u->first)) {

src/actions.cpp
@@ -588,8 +588,8 @@ battle_context::unit_stats::unit_stats(const unit &u, const map_location& u_loc,
 	weapon(0),
 	attack_num(u_attack_num),
 	is_attacker(attacking),
-	is_poisoned(utils::string_bool(u.get_state("poisoned"))),
-	is_slowed(utils::string_bool(u.get_state("slowed"))),
+	is_poisoned(u.get_state(unit::STATE_POISONED)),
+	is_slowed(u.get_state(unit::STATE_SLOWED)),
 	slows(false),
 	drains(false),
 	petrifies(false),
@@ -644,7 +644,7 @@ battle_context::unit_stats::unit_stats(const unit &u, const map_location& u_loc,
 		slows = weapon->get_special_bool("slow");
 		drains = weapon->get_special_bool("drains") && !utils::string_bool(opp.get_state("not_living"));
 		petrifies = weapon->get_special_bool("petrifies");
-		poisons = weapon->get_special_bool("poison") && utils::string_bool(opp.get_state("not_living")) != true && utils::string_bool(opp.get_state("poisoned")) != true;
+		poisons = weapon->get_special_bool("poison") && utils::string_bool(opp.get_state("not_living")) != true && opp.get_state(unit::STATE_POISONED) != true;
 		backstab_pos = is_attacker && backstab_check(u_loc, opp_loc, units, teams);
 		rounds = weapon->get_specials("berserk").highest("value", 1).first;
 		firststrike = weapon->get_special_bool("firststrike");
@@ -939,11 +939,11 @@ attack::attack(game_display& gui, const gamemap& map,
 	VALIDATE(attack_with < static_cast<int>(a_.get_unit().attacks().size()),
 		_("An invalid attacker weapon got selected."));
 	a_.get_unit().set_movement(a_.get_unit().movement_left()-a_.get_unit().attacks()[attack_with].movement_used());
-	a_.get_unit().set_state("not_moved","");
+	a_.get_unit().set_state(unit::STATE_NOT_MOVED,false);
 	d_.get_unit().set_resting(false);
 
 	// If the attacker was invisible, she isn't anymore!
-	a_.get_unit().set_state("hidden","");
+	a_.get_unit().set_state(unit::STATE_HIDDEN,false);
 
 	bc_ = new battle_context(map_, teams_, units_, state_, a_.loc_, d_.loc_, a_.weapon_, d_.weapon_);
 	a_stats_ = &bc_->get_attacker_stats();
@@ -1059,12 +1059,12 @@ attack::attack(game_display& gui, const gamemap& map,
 				std::string float_text = "";
 				if(hits) {
 					if (a_stats_->poisons &&
-							!utils::string_bool(d_.get_unit().get_state("poisoned"))) {
+							!(d_.get_unit().get_state(unit::STATE_POISONED))) {
 						float_text += (d_.get_unit().gender() == unit_race::FEMALE ?  _("female^poisoned") : _("poisoned"));
 						float_text += '\n';
 					}
 
-					if(a_stats_->slows && !utils::string_bool(d_.get_unit().get_state("slowed"))) {
+					if(a_stats_->slows && !(d_.get_unit().get_state(unit::STATE_SLOWED))) {
 						float_text += (d_.get_unit().gender() == unit_race::FEMALE ?  _("female^slowed") : _("slowed"));
 						float_text += '\n';
 					}
@@ -1244,13 +1244,13 @@ attack::attack(game_display& gui, const gamemap& map,
 				break;
 			} else if(hits) {
 				if (a_stats_->poisons &&
-						!utils::string_bool(d_.get_unit().get_state("poisoned"))) {
-					d_.get_unit().set_state("poisoned","yes");
+						!(d_.get_unit().get_state(unit::STATE_POISONED))) {
+					d_.get_unit().set_state(unit::STATE_POISONED,true);
 					LOG_NG << "defender poisoned\n";
 				}
 
-				if(a_stats_->slows && !utils::string_bool(d_.get_unit().get_state("slowed"))) {
-					d_.get_unit().set_state("slowed","yes");
+				if(a_stats_->slows && !(d_.get_unit().get_state(unit::STATE_SLOWED))) {
+					d_.get_unit().set_state(unit::STATE_SLOWED,true);
 					update_def_fog = true;
 					d_.damage_ = d_stats_->slow_damage;
 					LOG_NG << "defender slowed\n";
@@ -1259,7 +1259,7 @@ attack::attack(game_display& gui, const gamemap& map,
 				// If the defender is petrified, the fight stops immediately
 				static const std::string petrify_string("petrified");
 				if (a_stats_->petrifies) {
-					d_.get_unit().set_state("petrified","yes");
+					d_.get_unit().set_state(unit::STATE_PETRIFIED,true);
 					update_def_fog = true;
 					a_.n_attacks_ = 0;
 					d_.n_attacks_ = 0;
@@ -1334,17 +1334,17 @@ attack::attack(game_display& gui, const gamemap& map,
 				std::string float_text = "";
 				if(hits) {
 					if (d_stats_->poisons &&
-							!utils::string_bool(a_.get_unit().get_state("poisoned"))) {
+							!(a_.get_unit().get_state(unit::STATE_POISONED))) {
 						float_text += (a_.get_unit().gender() == unit_race::FEMALE ?  _("female^poisoned") : _("poisoned"));
 						float_text += '\n';
 					}
 
-					if(d_stats_->slows && !utils::string_bool(a_.get_unit().get_state("slowed"))) {
+					if(d_stats_->slows && !(a_.get_unit().get_state(unit::STATE_SLOWED))) {
 						float_text += (a_.get_unit().gender() == unit_race::FEMALE ?  _("female^slowed") : _("slowed"));
 						float_text += '\n';
 					}
 
-					// If the defender is petrified, the fight stops immediately
+					// If the attacker is petrified, the fight stops immediately
 					static const std::string petrify_string("petrified");
 					if (d_stats_->petrifies) {
 						float_text += (a_.get_unit().gender() == unit_race::FEMALE ?  _("female^petrified") : _("petrified"));
@@ -1513,13 +1513,13 @@ attack::attack(game_display& gui, const gamemap& map,
 				break;
 			} else if(hits) {
 				if (d_stats_->poisons &&
-						!utils::string_bool(a_.get_unit().get_state("poisoned"))) {
-					a_.get_unit().set_state("poisoned","yes");
+						!(a_.get_unit().get_state(unit::STATE_POISONED))) {
+					a_.get_unit().set_state(unit::STATE_POISONED,true);
 					LOG_NG << "attacker poisoned\n";
 				}
 
-				if(d_stats_->slows && !utils::string_bool(a_.get_unit().get_state("slowed"))) {
-					a_.get_unit().set_state("slowed","yes");
+				if(d_stats_->slows && !(a_.get_unit().get_state(unit::STATE_SLOWED))) {
+					a_.get_unit().set_state(unit::STATE_SLOWED,true);
 					update_att_fog = true;
 					a_.damage_ = a_stats_->slow_damage;
 					LOG_NG << "attacker slowed\n";
@@ -1529,7 +1529,7 @@ attack::attack(game_display& gui, const gamemap& map,
 				// If the attacker is petrified, the fight stops immediately
 				static const std::string petrify_string("petrified");
 				if (d_stats_->petrifies) {
-					a_.get_unit().set_state("petrified","yes");
+					a_.get_unit().set_state(unit::STATE_PETRIFIED,true);
 					update_att_fog = true;
 					d_.n_attacks_ = 0;
 					a_.n_attacks_ = 0;
@@ -1707,7 +1707,7 @@ void calculate_healing(game_display& disp, const gamemap& map,
 
 		unit_ability_list heal = i->second.get_abilities("heals",i->first);
 
-		const bool is_poisoned = utils::string_bool(i->second.get_state("poisoned"));
+		const bool is_poisoned = i->second.get_state(unit::STATE_POISONED);
 		if(is_poisoned) {
 			// Remove the enemies' healers to determine if poison is slowed or cured
 			for (std::vector<std::pair<const config *, map_location> >::iterator
@@ -1799,7 +1799,7 @@ void calculate_healing(game_display& disp, const gamemap& map,
 		}
 		if(is_poisoned) {
 			if(curing == "cured") {
-				i->second.set_state("poisoned","");
+				i->second.set_state(unit::STATE_POISONED,false);
 				healing = rest_healing;
 				healers.clear();
 				if (curer != units.end())
@@ -2119,7 +2119,7 @@ namespace {
 				//just in case we managed to move on a fogged hex (teleport)
 				if(seen_units != NULL && known_units != NULL
 						&& known_units->count(*it) == 0 && *it != loc) {
-					if (!utils::string_bool(sighted->second.get_state("petrified")))
+					if (!(sighted->second.get_state(unit::STATE_PETRIFIED)))
 					{
 						seen_units->insert(*it);
 					}

src/ai/ai.cpp
@@ -856,7 +856,7 @@ bool ai::get_healing(std::map<map_location,paths>& possible_moves,
 		// then try to find a vacant village for it to rest in.
 		if(u.side() == get_side() &&
 		   (u.max_hitpoints() - u.hitpoints() >= game_config::poison_amount/2
-		   || utils::string_bool(u.get_state("poisoned"))) &&
+		   || u.get_state(unit::STATE_POISONED)) &&
 		   !u.get_ability_bool("regenerate",u_it->first)) {
 
 			// Look for the village which is the least vulnerable to enemy attack.

src/ai/ai_attack.cpp
@@ -423,7 +423,7 @@ void ai::attack_analysis::analyze(const gamemap& map, unit_map& units,
 		avg_losses += cost * prob_died;
 
 		// add half of cost for poisoned unit so it might get chance to heal
-		avg_losses += cost * utils::string_bool(up->second.get_state("poisoned")) /2;
+		avg_losses += cost * up->second.get_state(unit::STATE_POISONED) /2;
 
 		// Double reward to emphasize getting onto villages if they survive.
 		if (on_village) {

src/ai/contexts.cpp
@@ -211,7 +211,7 @@ map_location ai_readwrite_context::move_unit(map_location from, map_location to,
 	if(u != get_info().units.end()) {
 		if(u->second.movement_left()==u->second.total_movement()) {
 			u->second.set_movement(0);
-			u->second.set_state("not_moved","yes");
+			u->second.set_state(unit::STATE_NOT_MOVED,true);
 		} else if (from == loc) {
 			u->second.set_movement(0);
 		}

src/game_events.cpp
@@ -661,9 +661,9 @@ WML_HANDLER_FUNCTION(unpetrify, /*event_info*/, cfg)
 		std::vector<bool> clear_fog_side(rsrc.teams->size(), false);
 
 		for(unit_map::iterator i = rsrc.units->begin(); i != rsrc.units->end(); ++i) {
-			if(utils::string_bool(i->second.get_state("petrified"))) {
+			if(i->second.get_state(unit::STATE_PETRIFIED)) {
 				if(filter.null() || game_events::unit_matches_filter(i, filter)) {
-					i->second.set_state("petrified","");
+					i->second.set_state(unit::STATE_PETRIFIED,false);
 					clear_fog_side[i->second.side()-1] = true;
 				}
 			}

src/generate_report.cpp
@@ -98,19 +98,19 @@ report generate_report(TYPE type,
 			tooltip << _("invisible: ") << _("This unit is invisible. It cannot be seen or attacked by enemy units.");
 			res.add_image(unit_status,tooltip);
 		}
-		if(utils::string_bool(u->second.get_state("slowed"))) {
+		if(u->second.get_state(unit::STATE_SLOWED)) {
 			unit_status << "misc/slowed.png";
 			tooltip << _("slowed: ") << _("This unit has been slowed. It will only deal half its normal damage when attacking and its movement cost is doubled.");
 			res.add_image(unit_status,tooltip);
 		}
-		if(utils::string_bool(u->second.get_state("poisoned"))) {
+		if(u->second.get_state(unit::STATE_POISONED)) {
 			unit_status << "misc/poisoned.png";
 			tooltip << _("poisoned: ") << _("This unit is poisoned. It will lose 8 HP every turn until it can seek a cure to the poison in a village or from a friendly unit with the 'cures' ability.\n\
 \n\
 Units cannot be killed by poison alone. The poison will not reduce it below 1 HP.");
 			res.add_image(unit_status,tooltip);
 		}
-		if(utils::string_bool(u->second.get_state("petrified"))) {
+		if(u->second.get_state(unit::STATE_PETRIFIED)) {
 			unit_status << "misc/petrified.png";
 			tooltip << _("petrified: ") << _("This unit has been petrified. It may not move or attack.");
 			res.add_image(unit_status,tooltip);
@@ -231,7 +231,7 @@ Units cannot be killed by poison alone. The poison will not reduce it below 1 HP
 			const std::string& lang_type = gettext(at_it->type().c_str());
 			str.str("");
 			str << "<245,230,193>";
-			if(utils::string_bool(u->second.get_state("slowed"))) {
+			if(u->second.get_state(unit::STATE_SLOWED)) {
 				str << round_damage(at_it->damage(),1,2) << "-" ;
 			} else {
 				str << at_it->damage() << "-" ;
@@ -254,7 +254,7 @@ Units cannot be killed by poison alone. The poison will not reduce it below 1 HP
 			str << " " << at_it->name() << " " << at_it->accuracy_parry_description();
 			tooltip << at_it->name() << "\n";
 			int effdmg;
-			if(utils::string_bool(u->second.get_state("slowed"))) {
+			if(u->second.get_state(unit::STATE_SLOWED)) {
 				effdmg = round_damage(at_it->damage(),1,2);
 			} else {
 				effdmg = at_it->damage();

src/menu_events.cpp
@@ -282,11 +282,11 @@ namespace events{
 			row << i->first << COLUMN_SEPARATOR;
 
 			// show icons if unit is slowed, poisoned, petrified, invisible:
-			if(utils::string_bool(i->second.get_state("petrified")))
+			if(i->second.get_state(unit::STATE_PETRIFIED))
 				row << IMAGE_PREFIX << "misc/petrified.png"    << IMG_TEXT_SEPARATOR;
-			if(utils::string_bool(i->second.get_state("slowed")))
+			if(i->second.get_state(unit::STATE_SLOWED))
 				row << IMAGE_PREFIX << "misc/slowed.png"   << IMG_TEXT_SEPARATOR;
-			if(utils::string_bool(i->second.get_state("poisoned")))
+			if(i->second.get_state(unit::STATE_POISONED))
 				row << IMAGE_PREFIX << "misc/poisoned.png" << IMG_TEXT_SEPARATOR;
 
 			/** @todo FIXME: condition for "invisible" does not work */

commit 908c11750b52c3bd44d3de1a08a30bd3f2412b27

Fixed bug introduced by r35630 - interpreting any non-empty value of boolean states as "true". Fixes problem with 'all enemy units on Dark Forecast start poisoned'
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@35755 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -1033,7 +1033,7 @@ void unit::set_state(const std::string& state, const std::string& value)
 		}
 	} else {
 		if (known_boolean_state_id!=STATE_UNKNOWN){
-			set_state(known_boolean_state_id,true);
+			set_state(known_boolean_state_id,utils::string_bool(value));
 			return;
 		}
 		states_[state] = value;