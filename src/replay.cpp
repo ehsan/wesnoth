ehsan/wesnoth

commit 13ac9c7ab2f71ead194a707be127330c6cf27531

Added detailed kill-event logging to replays. initial work on parsing those events from replays into upload_logs. added hook read_replay() to read replays after scenario end but before the upload_log deconstructor is called.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@37560 75b3775d-3801-0410-9e12-e2e43121521d

src/replay.cpp
@@ -250,7 +250,9 @@ void replay::add_movement(const std::vector<map_location>& steps)
 }
 
 void replay::add_attack(const map_location& a, const map_location& b,
-	int att_weapon, int def_weapon)
+	int att_weapon, int def_weapon, const std::string& attacker_type_id,
+	const std::string& defender_type_id, int attacker_lvl,
+	int defender_lvl, const size_t turn, const time_of_day t)
 {
 	add_pos("attack",a,b);
 	char buf[100];
@@ -259,6 +261,15 @@ void replay::add_attack(const map_location& a, const map_location& b,
 	cfg["weapon"] = buf;
 	snprintf(buf,sizeof(buf),"%d",def_weapon);
 	cfg["defender_weapon"] = buf;
+	cfg["attacker_type"] = attacker_type;
+	cfg["defender_type"] = defender_type;
+	snprintf(buf,sizeof(buf),"%d",attacker_lvl);
+	cfg["attacker_lvl"] = buf;
+	snprintf(buf,sizeof(buf),"%d",defender_lvl);
+	cfg["defender_lvl"] = buf;
+	snprintf(buf,sizeof(buf),"%d",turn);
+	cfg["turn"] = buf;
+	cfg["tod"] = t.id;
 	add_unit_checksum(a,current_);
 	add_unit_checksum(b,current_);
 }

src/actions.cpp
@@ -1184,7 +1184,7 @@ attack::attack(const map_location &attacker, const map_location &defender,
 				config cfg;
 				cfg["hits"] = (hits ? "yes" : "no");
 				cfg["dies"] = (dies ? "yes" : "no");
-
+				cfg["unit_hit"] = "defender";
 				cfg["damage"] = lexical_cast<std::string>(damage_defender_takes);
 				cfg["chance"] = lexical_cast<std::string>(a_.cth_);
 
@@ -1451,6 +1451,7 @@ attack::attack(const map_location &attacker, const map_location &defender,
 				config cfg;
 				cfg["hits"] = (hits ? "yes" : "no");
 				cfg["dies"] = (dies ? "yes" : "no");
+				cfg["unit_hit"] = "attacker";
 				cfg["damage"] = lexical_cast<std::string>(damage_attacker_takes);
 				cfg["chance"] = lexical_cast<std::string>(d_.cth_);
 

src/ai/actions.cpp
@@ -300,8 +300,14 @@ void attack_result::do_execute()
 		set_error(E_UNABLE_TO_CHOOSE_ATTACKER_WEAPON);
 		return;
 	}
+	
+	const unit *d_ = get_unit(get_info(),defender_loc_);
+	const unit *a_ = get_unit(get_info(),attacker_loc_);
 
-	recorder.add_attack(attacker_loc_, defender_loc_, attacker_weapon, defender_weapon);
+	//@TODO: change ToD to be location specific for the defender unit
+	recorder.add_attack(attacker_loc_, defender_loc_, attacker_weapon, defender_weapon, a_->type_id(),
+		d_->type_id(), a_->level(), d_->level(), resources::tod_manager->turn(), 
+		resources::tod_manager->get_time_of_day());
 	rand_rng::invalidate_seed();
 	rand_rng::clear_new_seed_callback();
 	while (!rand_rng::has_valid_seed()) {

src/mouse_events.cpp
@@ -29,7 +29,9 @@
 #include "menu_events.hpp"
 #include "sound.hpp"
 #include "replay.hpp"
+#include "resources.hpp"
 #include "rng.hpp"
+#include "tod_manager.hpp"
 #include "wml_separators.hpp"
 
 #include <boost/bind.hpp>
@@ -675,7 +677,10 @@ bool mouse_handler::attack_enemy_(unit_map::iterator attacker, unit_map::iterato
 		gui().unhighlight_reach();
 
 		gui().draw();
-		recorder.add_attack(attacker_loc, defender_loc, att.attack_num, def.attack_num);
+		//@TODO: change ToD to be location specific for the defender
+		recorder.add_attack(attacker_loc, defender_loc, att.attack_num, def.attack_num, 
+			attacker->second.type_id(), defender->second.type_id(), att.level, 
+			def.level, resources::tod_manager->turn(), resources::tod_manager->get_time_of_day());
 		rand_rng::invalidate_seed();
 		if (rand_rng::has_valid_seed()) { //means SRNG is disabled
 			perform_attack(attacker_loc, defender_loc, att.attack_num, def.attack_num, rand_rng::get_last_seed());

src/playcampaign.cpp
@@ -35,6 +35,7 @@
 #include "sound.hpp"
 #include "wml_exception.hpp"
 #include "formula_string_utils.hpp"
+#include "upload_log.hpp"
 
 #define LOG_G LOG_STREAM(info, lg::general)
 
@@ -118,6 +119,7 @@ static LEVEL_RESULT playsingle_scenario(const config& game_config,
 	LOG_NG << "created objects... " << (SDL_GetTicks() - playcontroller.get_ticks()) << "\n";
 
 	const LEVEL_RESULT res = playcontroller.play_scenario(story, log, skip_replay, end_level);
+	log.read_replay();
 
 	if (res == DEFEAT) {
 		gui2::show_transient_message(disp.video(),
@@ -149,7 +151,7 @@ static LEVEL_RESULT playmp_scenario(const config& game_config,
 	playmp_controller playcontroller(*level, state_of_game, ticks, num_turns,
 		game_config, disp.video(), skip_replay, io_type == IO_SERVER);
 	const LEVEL_RESULT res = playcontroller.play_scenario(story, log, skip_replay, end_level);
-
+	log.read_replay();
 	//Check if the player started as mp client and changed to host
 	if (io_type == IO_CLIENT && playcontroller.is_host())
 		io_type = IO_SERVER;

src/replay.hpp
@@ -49,7 +49,9 @@ public:
 	void add_countdown_update(int value,int team);
 	void add_movement(const std::vector<map_location>& steps);
 	void add_attack(const map_location& a, const map_location& b,
-	                int att_weapon, int def_weapon);
+		int att_weapon, int def_weapon, const std::string& attacker_type_id,
+		const std::string& defender_type_id, int attacker_lvl,
+		int defender_lvl, const size_t turn, const time_of_day t);
 	void add_seed(const char* child_name, rand_rng::seed_t seed);
 	void choose_option(int index);
 	void text_input(std::string input);

src/upload_log.cpp
@@ -24,10 +24,12 @@
 
 #include "construct_dialog.hpp"
 #include "filesystem.hpp"
+#include "foreach.hpp"
 #include "game_preferences.hpp"
 #include "gamestatus.hpp"
 #include "gettext.hpp"
 #include "log.hpp"
+#include "replay.hpp"
 #include "serialization/parser.hpp"
 #include "unit.hpp"
 #include "unit_map.hpp"
@@ -39,6 +41,8 @@
 static lg::log_domain log_uploader("uploader");
 #define DBG_UPLD LOG_STREAM(debug, log_uploader)
 #define ERR_UPLD LOG_STREAM(err, log_uploader)
+
+extern replay recorder;
 
 namespace uploader_settings {
 	bool new_uploader = false;
@@ -252,6 +256,22 @@ upload_log::upload_log(bool enable) :
 			thread_.t = new threading::thread(upload_logs_dev, &thread_);
 		} else {
 			thread_.t = new threading::thread(upload_logs, &thread_);
+		}
+	}
+}
+
+void upload_log::read_replay()
+{
+	if(enabled_ && !config_.empty() && !game_config::debug) {
+		foreach (const config &c, recorder.get_replay_data().child_range("command")) {
+			if(c.has_attribute("attack")) {
+				//search through the attack to see if a unit died
+				foreach (const config &c2, c.child_range("random")) {
+					if(c2.has_attribute("dies") && c2["dies"] == "yes") {
+						//config_.add_child("kill_event",c);
+					}
+				}	
+			}
 		}
 	}
 }

src/upload_log.hpp
@@ -54,6 +54,7 @@ public:
 	void defeat(int turn);
 	void victory(int turn, int gold);
 	void quit(int turn);
+	void read_replay();
 
 	// Argument passed to upload thread.
 	struct thread_info {

commit 5ffa427731fa12958ba302f10f043d5368158d83

Fixed compiler error introduced by r37560
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@37565 75b3775d-3801-0410-9e12-e2e43121521d

src/replay.cpp
@@ -261,8 +261,8 @@ void replay::add_attack(const map_location& a, const map_location& b,
 	cfg["weapon"] = buf;
 	snprintf(buf,sizeof(buf),"%d",def_weapon);
 	cfg["defender_weapon"] = buf;
-	cfg["attacker_type"] = attacker_type;
-	cfg["defender_type"] = defender_type;
+	cfg["attacker_type"] = attacker_type_id;
+	cfg["defender_type"] = defender_type_id;
 	snprintf(buf,sizeof(buf),"%d",attacker_lvl);
 	cfg["attacker_lvl"] = buf;
 	snprintf(buf,sizeof(buf),"%d",defender_lvl);