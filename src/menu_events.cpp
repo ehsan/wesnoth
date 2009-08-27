ehsan/wesnoth

commit 054614299e7275e7d96008c6c3b33e2208d6c358

WIP on show_ai_moves
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@37801 75b3775d-3801-0410-9e12-e2e43121521d

src/menu_events.cpp
@@ -1540,7 +1540,7 @@ private:
 		assert(route.steps.front() == ui->first);
 
 		gui_->set_route(&route);
-		move_unit(NULL, route.steps, &recorder, &undo_stack_, NULL, continue_move);
+		move_unit(NULL, route.steps, &recorder, false, &undo_stack_, NULL, continue_move);
 		gui_->invalidate_game_status();
 	}
 

src/actions.cpp
@@ -2209,10 +2209,11 @@ bool clear_shroud(int side)
 }
 
 size_t move_unit(move_unit_spectator *move_spectator,
-		const std::vector<map_location> &route,
-		replay* move_recorder, undo_list* undo_stack,
-		map_location *next_unit, bool continue_move,
-		bool should_clear_shroud, bool is_replay)
+		 const std::vector<map_location> &route,
+		 replay* move_recorder, undo_list* undo_stack,
+		 bool show_move,
+		 map_location *next_unit, bool continue_move,
+		 bool should_clear_shroud, bool is_replay)
 {
 	assert(route.empty() == false);
 
@@ -2394,11 +2395,14 @@ size_t move_unit(move_unit_spectator *move_spectator,
 	if (next_unit != NULL)
 		*next_unit = steps.back();
 
-	// Move the unit on the screen. Hide the unit in its current location,
-	// but don't actually remove it until the move is done,
-	// so that while the unit is moving status etc.
-	// will still display the correct number of units.
-	unit_display::move_unit(steps,ui->second,teams);
+	//@todo reenable after setting proper invalidation rules
+	//if (show_move) {
+		// Move the unit on the screen. Hide the unit in its current location,
+		// but don't actually remove it until the move is done,
+		// so that while the unit is moving status etc.
+		// will still display the correct number of units.
+		unit_display::move_unit(steps,ui->second,teams);
+	//}
 
 	ui->second.set_movement(moves_left);
 
@@ -2423,6 +2427,7 @@ size_t move_unit(move_unit_spectator *move_spectator,
 			event_mutated = get_village(steps.back(), disp, teams, team_num, units, &action_time_bonus);
 		}
 	}
+
 
 	// Show the final move animation step
 	disp.draw();

src/actions.hpp
@@ -392,6 +392,7 @@ typedef std::deque<undo_action> undo_list;
 size_t move_unit(move_unit_spectator* move_spectator,
 	const std::vector<map_location> &steps,
 				replay* move_recorder, undo_list* undos,
+				bool show_move,
 				map_location *next_unit = NULL,
 				bool continue_move = false, bool should_clear_shroud=true, bool is_replay=false);
 

src/ai/actions.cpp
@@ -490,6 +490,7 @@ void move_result::do_execute()
 			/*std::vector<map_location> route*/ route_.steps,
 			/*replay* move_recorder*/ &recorder,
 			/*undo_list* undo_stack*/ NULL,
+			/*bool show_move*/ preferences::show_ai_moves(),
 			/*map_location *next_unit*/ NULL,
 			/*bool continue_move*/ true, //@todo: 1.7 set to false after implemeting interrupt awareness
 			/*bool should_clear_shroud*/ true,

src/mouse_events.cpp
@@ -525,7 +525,7 @@ bool mouse_handler::move_unit_along_current_route(bool check_shroud, bool attack
 	attackmove_ = attackmove;
 	size_t moves = 0;
 	try{
-		moves = ::move_unit(NULL, steps, &recorder, &undo_stack_, &next_unit_, false, check_shroud);
+		moves = ::move_unit(NULL, steps, &recorder, &undo_stack_, true, &next_unit_, false, check_shroud);
 	} catch(end_turn_exception&) {
 		attackmove_ = false;
 		cursor::set(cursor::NORMAL);

src/replay.cpp
@@ -1039,7 +1039,8 @@ bool do_replay_handle(int side_num, const std::string &do_untill)
 				replay::throw_error(errbuf.str());
 			}
 
-			::move_unit(NULL, steps, NULL, NULL, NULL, true, true, true);
+			bool show_move = preferences::show_ai_moves() || !(current_team.is_ai() || current_team.is_network_ai()); 
+			::move_unit(NULL, steps, NULL, NULL, show_move, NULL, true, true, true);
 
 			//NOTE: The AI fire sighetd event whem moving in the FoV of team 1
 			// (supposed to be the human player in SP)

commit 1fb7eacbeef2328496afe38b1b6c413a748b83d3

Fix broken undo for goto, caused by a typo in r37801
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38130 75b3775d-3801-0410-9e12-e2e43121521d

src/menu_events.cpp
@@ -1600,7 +1600,7 @@ private:
 		assert(route.steps.front() == ui->first);
 
 		gui_->set_route(&route);
-		move_unit(NULL, route.steps, &recorder, false, &undo_stack_, NULL, continue_move);
+		move_unit(NULL, route.steps, &recorder, &undo_stack_, false, NULL, continue_move);
 		gui_->invalidate_game_status();
 	}
 