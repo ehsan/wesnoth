ehsan/wesnoth

commit 11768e3a3ca3e1881e2d3c1c985639861cc496df

Enabled log uploads for ai vs. ai multiplayer matches (logs are still incomplete, however). refactored read_replay(). Wrote new upload_log.start() without deprecated parameters.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@37610 75b3775d-3801-0410-9e12-e2e43121521d

src/playsingle_controller.cpp
@@ -324,6 +324,8 @@ LEVEL_RESULT playsingle_controller::play_scenario(
 		if (first_human_team_ != -1) {
 			log.start(gamestate_, teams_[first_human_team_], first_human_team_ + 1, units_,
 					  loading_game_ ? gamestate_.get_variable("turn_number") : "", number_of_turns(), resources::game_map->write());
+		} else { //ai vs. ai upload logs
+			log.start(gamestate_, resources::game_map->write());
 		}
 
 		fire_prestart(!loading_game_);
@@ -376,6 +378,7 @@ LEVEL_RESULT playsingle_controller::play_scenario(
 		throw;
 	} catch(end_level_exception& end_level) {
 		ai_testing::log_game_end();
+		log.quit(turn());
 		*end_level_result = end_level;
 		if(!end_level.custom_endlevel_music.empty()) {
 			switch(end_level.result) {

src/upload_log.cpp
@@ -262,7 +262,7 @@ upload_log::upload_log(bool enable) :
 
 void upload_log::read_replay()
 {
-	if( !uploader_settings::new_uploader ) {
+	if( !uploader_settings::new_uploader || !enabled_ || !game_config::debug ) {
 		return;
 	}
 
@@ -271,18 +271,16 @@ void upload_log::read_replay()
 		game_ = new config();
 	}
 
-	if(enabled_ && !game_config::debug) {
-		foreach (const config &c, recorder.get_replay_data().child_range("command")) {
-			if(c.child_count("attack")) {
-				//search through the attack to see if a unit died
-				foreach (const config &c2, c.child_range("random")) {
-					if(c2.child_count("results") && c2.child("results")["dies"] == "yes") {
-						config& cfg = game_->add_child("kill_event");
-						cfg.add_child("attack",c.child("attack"));
-						cfg.add_child("results",c2.child("results"));
-					}
-				}	
-			}
+	foreach (const config &c, recorder.get_replay_data().child_range("command")) {
+		if(c.child_count("attack")) {
+			//search through the attack to see if a unit died
+			foreach (const config &c2, c.child_range("random")) {
+				if(c2.child_count("results") && c2.child("results")["dies"] == "yes") {
+					config& cfg = game_->add_child("kill_event");
+					cfg.add_child("attack",c.child("attack"));
+					cfg.add_child("results",c2.child("results"));
+				}
+			}	
 		}
 	}
 }
@@ -453,6 +451,34 @@ void upload_log::start(game_state &state, const team &team,
 			}
 		}
 	}
+}
+
+void upload_log::start(game_state &state, const std::string map_data)
+{
+	// If we have a previous game which is finished, add it.
+	if (game_finished(game_)) {
+		config_.add_child("game", *game_);
+	}
+
+	// Start could be called more than once,
+	// so delete game_ to prevent memory leak
+	delete game_;
+	game_ = new config();
+	(*game_)["time"] = lexical_cast<std::string>(SDL_GetTicks() / 1000);
+	(*game_)["campaign"] = state.classification().campaign_define;
+	(*game_)["difficulty"] = state.classification().difficulty;
+	(*game_)["scenario"] = state.classification().scenario;
+	if(uploader_settings::new_uploader) {
+		//replace newlines in map definition with semicolons so that braindead server-side wml parser doesn't get confused
+		std::string encoded_map(map_data);
+		for(size_t idx = 0; idx < encoded_map.length(); idx++) {
+			if(encoded_map[idx] == '\n')
+				encoded_map[idx] = ';';
+		}
+		(*game_)["map_data"] = encoded_map;
+	}
+	if (!state.classification().version.empty())
+		(*game_)["version"] = state.classification().version;
 }
 
 // User finishes a scenario.

src/game.cpp
@@ -917,9 +917,16 @@ bool game_controller::play_multiplayer_mode()
 	level.add_child("era", era_cfg);
 
 	try {
-		upload_log nolog(false);
+		//check if all sides are AI and we are using new uploader -> log these games
+		bool all_ai = true;
+		foreach (config &s, level.child_range("side")) {
+			if( s["controller"] != "ai" ) {
+				all_ai = false;
+			}	
+		}
+		upload_log log( all_ai && uploader_settings::new_uploader );
 		state_.snapshot = level;
-		play_game(disp(),state_,game_config_,nolog);
+		play_game(disp(),state_,game_config_,log);
 	} catch(game::error& e) {
 		std::cerr << "caught error: '" << e.message << "'\n";
 	} catch(game::load_game_exception& e) {

src/upload_log.hpp
@@ -49,6 +49,7 @@ public:
 	void start(game_state &state, const team &team,
 	           int side_number, const unit_map &map, const t_string &turn,
 			   int num_turns, const std::string map_data);
+	void start(game_state &state, const std::string map_data);
 
 	// User finishes a level.
 	void defeat(int turn);

commit 9ba1004916a0be53a398853068647f65a91eb0ad

Fixed bug introduced in r37610: upload log always contains a [quit] child, even during victory and draw.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38311 75b3775d-3801-0410-9e12-e2e43121521d

src/playsingle_controller.cpp
@@ -382,7 +382,6 @@ LEVEL_RESULT playsingle_controller::play_scenario(
 		throw;
 	} catch(end_level_exception& end_level) {
 		ai_testing::log_game_end();
-		log.quit(turn());
 		*end_level_result = end_level;
 		if(!end_level.custom_endlevel_music.empty()) {
 			switch(end_level.result) {

src/upload_log.cpp
@@ -349,7 +349,7 @@ bool upload_log::game_finished(config *game)
 	if (!game)
 		return false;
 
-	return game->child("victory") || game->child("defeat") || game->child("quit");
+	return game->child("victory") || game->child("defeat") || game->child("quit") || game->child("upload_log").child("ai_log");
 }
 
 config &upload_log::add_game_result(const std::string &str, int turn)