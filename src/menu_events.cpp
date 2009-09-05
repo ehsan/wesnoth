ehsan/wesnoth

commit 78f3f3fceb6dedf5d82cc31543a0b12e39778ac6

removed :q alias for :quit
there is still :q! and it conflicts with the /q alias for /query

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@37945 75b3775d-3801-0410-9e12-e2e43121521d

src/menu_events.cpp
@@ -2270,7 +2270,6 @@ private:
 				register_command("save", &console_handler::do_save, _("Save game."));
 				register_alias("save", "w");
 				register_command("quit", &console_handler::do_quit, _("Quit game."));
-				register_alias("quit", "q");
 				register_alias("quit", "q!");
 				register_command("save_quit", &console_handler::do_save_quit,
 					_("Save and quit."));

commit 29c043d33594ba5b62bfee9ddf976f4d0c3e265f

Fixed the unit test 'hanging'.
The problem was introduced in r37945 added some comment to avoid it from
happening again.

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@38434 75b3775d-3801-0410-9e12-e2e43121521d

src/menu_events.cpp
@@ -2328,6 +2328,7 @@ private:
 				register_command("save", &console_handler::do_save, _("Save game."));
 				register_alias("save", "w");
 				register_command("quit", &console_handler::do_quit, _("Quit game."));
+				// Note the next value is used hardcoded in the init tests.
 				register_alias("quit", "q!");
 				register_command("save_quit", &console_handler::do_save_quit,
 					_("Save and quit."));

src/tests/utils/play_scenario.cpp
@@ -107,6 +107,7 @@ namespace test_utils {
 
 		source_.type_key(current_time_++, SDLK_COLON, SDLMod(KMOD_LSHIFT | KMOD_SHIFT) );
 		source_.type_key(current_time_++, SDLK_q);
+		source_.type_key(current_time_++, SDLK_EXCLAIM);
 		source_.type_key(current_time_++, SDLK_RETURN);
 
 		upload_log no_upload(false);