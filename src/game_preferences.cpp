ehsan/wesnoth

commit ad51fac94307e7c8643cc60ed5b2308e14e3ab1b

Avoided costly roundtrip through strings.
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@46388 75b3775d-3801-0410-9e12-e2e43121521d

src/game_preferences.cpp
@@ -475,7 +475,7 @@ std::string get_mp_server_program_name()
 
 bool random_start_time()
 {
-	return settings::use_random_start_time(preferences::get("mp_random_start_time"));
+	return preferences::get("mp_random_start_time", true);
 }
 
 void set_random_start_time(bool value)
@@ -485,7 +485,7 @@ void set_random_start_time(bool value)
 
 bool fog()
 {
-	return settings::use_fog(preferences::get("mp_fog"));
+	return preferences::get("mp_fog", true);
 }
 
 void set_fog(bool value)
@@ -495,7 +495,7 @@ void set_fog(bool value)
 
 bool shroud()
 {
-	return settings::use_shroud(preferences::get("mp_shroud"));
+	return preferences::get("mp_shroud", true);
 }
 
 void set_shroud(bool value)

src/gui/dialogs/mp_create_game.cpp
@@ -165,9 +165,9 @@ void tmp_create_game::update_map_settings(twindow& window)
 		if(scenario_) {
 			const config &side = scenario_->child("side");
 
-			fog_->set_widget_value(window, ::settings::use_fog(side["fog"]));
-			shroud_->set_widget_value(window, ::settings::use_shroud(side["shroud"]));
-			start_time_->set_widget_value(window, ::settings::use_random_start_time((*scenario_)["random_start_time"]));
+			fog_->set_widget_value(window, side["fog"].to_bool(true));
+			shroud_->set_widget_value(window, side["shroud"].to_bool(true));
+			start_time_->set_widget_value(window, (*scenario_)["random_start_time"].to_bool(true));
 
 			turns_->set_widget_value(window, ::settings::get_turns((*scenario_)["turns"]));
 			gold_->set_widget_value(window, ::settings::get_village_gold(side["village_gold"]));

src/multiplayer_create.cpp
@@ -571,7 +571,7 @@ void create::process_event()
 			preferences::xp_modifier());
 
 		random_start_time_.set_check(map_settings ?
-			settings::use_random_start_time(parameters_.scenario_data["random_start_time"]) :
+			parameters_.scenario_data["random_start_time"].to_bool(true) :
 			preferences::random_start_time());
 
 		// These are per player, always show values of player 1.
@@ -591,11 +591,11 @@ void create::process_event()
 				preferences::village_gold());
 
 			fog_game_.set_check(map_settings ?
-				settings::use_fog(cfg["fog"]) :
+				cfg["fog"].to_bool(true) :
 				preferences::fog());
 
 			shroud_game_.set_check(map_settings ?
-				settings::use_shroud(cfg["shroud"]) :
+				cfg["shroud"].to_bool(true) :
 				preferences::shroud());
 		}
 

src/settings.cpp
@@ -50,20 +50,5 @@ int get_xp_modifier(const std::string& value)
 	return lexical_cast_in_range<int>(value, 70, 30, 200);
 }
 
-bool use_fog(const std::string& value)
-{
-	return utils::string_bool(value, true);
-}
-
-bool use_random_start_time(const std::string& value)
-{
-	return utils::string_bool(value, true);
-}
-
-bool use_shroud(const std::string& value)
-{
-	return utils::string_bool(value, false);
-}
-
 } // end namespace settings
 

src/settings.hpp
@@ -71,36 +71,6 @@ namespace settings {
 	 */
 	int get_xp_modifier(const std::string& value);
 
-	/**
-	 * Returns whether the fog of war should be used.
-	 * If no valid value is supplied, it will return a default.
-	 *
-	 * @param value         string containing a boolean value
-	 *
-	 * @returns             whether or not the fog of war should be used
-	 */
-	bool use_fog(const std::string& value);
-
-	/**
-	 * Returns whether the random start time feature should be used.
-	 * If no valid value is supplied, it will return a default.
-	 *
-	 * @param value         string containing a boolean value
-	 *
-	 * @returns             whether or not the random start time should be used
-	 */
-	bool use_random_start_time(const std::string& value);
-
-	/**
-	 * Returns whether the shroud should be used.
-	 * If no valid value is supplied, it will return a default.
-	 *
-	 * @param value         string containing a boolean value
-	 *
-	 * @returns             whether or not the shroud should be used
-	 */
-	bool use_shroud(const std::string& value);
-
 } // namespace settings
 
 #endif

commit a3ccd4509ba3e3eb5ad94903185aa848a04d516d

fixed regression from r46388; fixes bug #16866: Shroud is on by default in 'use map settings'
git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@47074 75b3775d-3801-0410-9e12-e2e43121521d

src/game_preferences.cpp
@@ -495,7 +495,7 @@ void set_fog(bool value)
 
 bool shroud()
 {
-	return preferences::get("mp_shroud", true);
+	return preferences::get("mp_shroud", false);
 }
 
 void set_shroud(bool value)

src/gui/dialogs/mp_create_game.cpp
@@ -166,7 +166,7 @@ void tmp_create_game::update_map_settings(twindow& window)
 			const config &side = scenario_->child("side");
 
 			fog_->set_widget_value(window, side["fog"].to_bool(true));
-			shroud_->set_widget_value(window, side["shroud"].to_bool(true));
+			shroud_->set_widget_value(window, side["shroud"].to_bool(false));
 			start_time_->set_widget_value(window, (*scenario_)["random_start_time"].to_bool(true));
 
 			turns_->set_widget_value(window, ::settings::get_turns((*scenario_)["turns"]));

src/multiplayer_create.cpp
@@ -595,7 +595,7 @@ void create::process_event()
 				preferences::fog());
 
 			shroud_game_.set_check(map_settings ?
-				cfg["shroud"].to_bool(true) :
+				cfg["shroud"].to_bool(false) :
 				preferences::shroud());
 		}
 