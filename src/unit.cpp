ehsan/wesnoth

commit 76e6ca73a3019acf38a711380a4a47fb347ae706

Harden unit_movement_resetter class.
Add a warning regarding its usage and test whether the unit exists
before assigning to it. (Still a bit experimental, but initial tests
show no regressions.)

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@51494 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -2799,6 +2799,14 @@ unit_movement_resetter::unit_movement_resetter(unit &u, bool operate) :
 
 unit_movement_resetter::~unit_movement_resetter()
 {
+	assert(resources::units);
+
+	/*
+	 * This assert should be safe, but not 100% sure so if it fails need
+	 * some extra validation code in this function.
+	 */
+	assert(resources::units->has_unit(&u_));
+
 	u_.movement_ = moves_;
 }
 

src/unit.hpp
@@ -467,8 +467,16 @@ private:
 	void clear_visibility_cache() const { invisibility_cache_.clear(); }
 };
 
-/** Object which temporarily resets a unit's movement */
+/**
+ * Object which temporarily resets a unit's movement.
+ *
+ * @warning
+ * The unit whose movement is reset may not be deleted while a
+ * @ref unit_movement_resetter object 'holds'. So best use it only in a small
+ * scope.
+ */
 struct unit_movement_resetter
+	: private boost::noncopyable
 {
 	unit_movement_resetter(unit& u, bool operate=true);
 	~unit_movement_resetter();

commit 62382eec492e40be4c3b4ddf469798891eacf2a2

Remove the assert introduced in r51494.
anonymissimus found some valid cases which now assert. Changed it in a
debug warning, mainly to have hint that it might be the cause of an
issue.

git-svn-id: http://svn.gna.org/svn/wesnoth/trunk@51506 75b3775d-3801-0410-9e12-e2e43121521d

src/unit.cpp
@@ -2801,12 +2801,13 @@ unit_movement_resetter::~unit_movement_resetter()
 {
 	assert(resources::units);
 
-	/*
-	 * This assert should be safe, but not 100% sure so if it fails need
-	 * some extra validation code in this function.
-	 */
-	assert(resources::units->has_unit(&u_));
-
+	if(!resources::units->has_unit(&u_)) {
+		/*
+		 * It might be valid that the unit is not in the unit map.
+		 * It might also mean a not longer valid unit will be assigned to.
+		 */
+		DBG_UT << "The unit to be removed is not in the unit map.\n";
+	}
 	u_.movement_ = moves_;
 }
 